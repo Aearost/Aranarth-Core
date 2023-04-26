3cce62f061863cf779505ac0b227afadaea7be36
Aearost <unconfigured@null.spigotmc.org>
2023-04-25 22:58:40 -0400

    Found horse stat minimums

--- src/com/aearost/aranarthcore/event/HorseSpawn.java
+++ src/com/aearost/aranarthcore/event/HorseSpawn.java
@@ -27,14 +27,17 @@ public class HorseSpawn implements Listener {
 		if (e.getEntity() instanceof Horse) {
 			Horse horse = (Horse) e.getEntity();
 
-			// A maximum limit of 30 hearts (60 half-hearts)
+			// A maximum limit of 30 hearts (60 half-hearts) --> 60
+			// Will need a minimum of 8 hearts (16 half-hearts)
 			horse.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(60);
 			
-			//A maximum limit of 8 blocks of jump
-			horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH).setBaseValue(1.28);
+			// A maximum limit of 8 blocks of jump --> 1.28
+			// A minimum limit of 2 blocks of jump --> 0.57
+			horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH).setBaseValue(0.57);
 			
-			// A maximum limit of 25 m/s
-			horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.592417062);
+			// A maximum limit of 25 m/s --> 0.592417062
+			// A minimum limit of 8 m/s --> 0.19
+			horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.19);
 			
 			// For testing only!!! Remove before final compile
 			horse.setAdult();
