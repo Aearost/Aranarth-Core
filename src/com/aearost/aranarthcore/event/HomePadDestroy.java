cba0bfa67af013e27148c77fba321542202eb57b
Aearost <unconfigured@null.spigotmc.org>
2023-06-20 10:37:01 -0400

    Added support for dropping/removing a homepad when breaking the block below a homepad

--- src/com/aearost/aranarthcore/event/HomePadDestroy.java
+++ src/com/aearost/aranarthcore/event/HomePadDestroy.java
@@ -8,6 +8,7 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 import com.aearost.aranarthcore.AranarthCore;
 import com.aearost.aranarthcore.items.HomePad;
@@ -28,10 +29,17 @@ public class HomePadDestroy implements Listener {
 	public void onHomePadDestroy(final BlockBreakEvent e) {
 		
 		Location location = e.getBlock().getLocation();
+		Location locationAbove = new Location(location.getWorld(), location.getX(), location.getY() + 1, location.getZ());
 		if (Objects.nonNull(AranarthUtils.getHomePad(location))) {
 			e.setCancelled(true);
 			AranarthUtils.removeHomePad(location);
 			location.getWorld().dropItemNaturally(location, HomePad.getHomePad());
+			location.getBlock().setType(Material.AIR);
+		} else if (Objects.nonNull(AranarthUtils.getHomePad(locationAbove))) {
+			AranarthUtils.removeHomePad(locationAbove);
+			locationAbove.getWorld().dropItemNaturally(locationAbove, HomePad.getHomePad());
+			locationAbove.getBlock().setType(Material.AIR);
+			location.getWorld().dropItemNaturally(location, new ItemStack(e.getBlock().getType()));
 			location.getBlock().setType(Material.AIR);
 		}
 		
