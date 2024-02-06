5bcf1d5abffe6ad131de3494baad20760d7d3a9a
Liam Harbec <liam.harbec13@gmail.com>
2024-02-06 18:52:50 -0500

    Blacklist functionality is complete

---
+++ src/com/aearost/aranarthcore/event/BlacklistItemPickupPrevent.java
@@ -0,0 +1,40 @@
+package com.aearost.aranarthcore.event;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+import com.aearost.aranarthcore.AranarthCore;
+import com.aearost.aranarthcore.utils.AranarthUtils;
+
+public class BlacklistItemPickupPrevent implements Listener {
+
+	public BlacklistItemPickupPrevent(AranarthCore plugin) {
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+
+	/**
+	 * Prevents players from picking up blacklisted items
+	 * 
+	 * @param e
+	 */
+	@EventHandler
+	public void onPlayerPickupItem(final EntityPickupItemEvent e) {
+		if (e.getEntity() instanceof Player) {
+			Player player = (Player) e.getEntity();
+			if (AranarthUtils.hasBlacklistedItems(player.getUniqueId())) {
+				List<ItemStack> blacklistedItems = AranarthUtils.getBlacklistedItems(player.getUniqueId());
+				for (ItemStack is : blacklistedItems) {
+					if (is.isSimilar(e.getItem().getItemStack())) {
+						e.setCancelled(true);
+					}
+				}
+			}
+		}
+	}
+}
