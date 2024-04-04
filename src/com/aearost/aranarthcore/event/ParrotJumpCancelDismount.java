84296571b74a371d68a7cd591e7fb47e68d3719f
BuildTools <unconfigured@null.spigotmc.org>
2024-04-03 21:54:26 -0400

    Added functionality to prevent parrot removal if not sneaking - this is untested

---
+++ src/com/aearost/aranarthcore/event/ParrotJumpCancelDismount.java
@@ -0,0 +1,40 @@
+package com.aearost.aranarthcore.event;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Parrot;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+
+import com.aearost.aranarthcore.AranarthCore;
+
+public class ParrotJumpCancelDismount implements Listener {
+
+	public ParrotJumpCancelDismount(AranarthCore plugin) {
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+
+	/**
+	 * Prevents parrots from being removed when jumping unless the player is sneaking.
+	 * 
+	 * @param e
+	 */
+	@EventHandler
+	public void onPlayerJump(final CreatureSpawnEvent e) {
+		if (e.getEntityType() == EntityType.PARROT) {
+			if (e.getSpawnReason() == SpawnReason.SHOULDER_ENTITY) {
+				Parrot parrot = (Parrot) e.getEntity();
+				Player player = (Player) parrot.getOwner();
+				// Only prevent removal if the player is sneaking
+				if (!player.isSneaking()) {
+					e.setCancelled(true);
+				}
+			}
+		}
+		
+	}
+
+}
