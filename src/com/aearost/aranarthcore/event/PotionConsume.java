41a6ed99043c709ef9fc9140a189511ff50ca63b
BuildTools <unconfigured@null.spigotmc.org>
2024-05-11 17:01:40 -0400

    Set up criteria for potion throw events to be triggered, must add auto-replace logic

--- src/com/aearost/aranarthcore/event/PotionConsume.java
+++ src/com/aearost/aranarthcore/event/PotionConsume.java
@@ -1,10 +1,13 @@
 package com.aearost.aranarthcore.event;
 
+import java.util.Objects;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.LingeringPotionSplashEvent;
-import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 import com.aearost.aranarthcore.AranarthCore;
@@ -33,19 +36,15 @@ public class PotionConsume implements Listener {
 	 *
 	 */
 	@EventHandler
-	public void onPotionUse(final PotionSplashEvent e) {
-		System.out.println("B");
-	}
-	
-	/**
-	 * Handles the auto-refill functionality when throwing of lingering potions.
-	 * 
-	 * @author Aearost
-	 *
-	 */
-	@EventHandler
-	public void onPotionUse(final LingeringPotionSplashEvent e) {
-		System.out.println("C");
+	public void onPotionUse(final PlayerInteractEvent e) {
+		if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (Objects.nonNull(e.getItem())) {
+				if (e.getItem().getType() == Material.SPLASH_POTION
+						|| e.getItem().getType() == Material.LINGERING_POTION) {
+					
+				}
+			}
+		}
 	}
 
 }
