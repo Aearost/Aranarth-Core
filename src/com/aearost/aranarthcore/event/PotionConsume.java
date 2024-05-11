18a7dd4f1eafda90f66c333b462dde2f5d18f170
BuildTools <unconfigured@null.spigotmc.org>
2024-05-11 17:11:07 -0400

    Potions are now detected if in inventory, must finalize replace functionality

--- src/com/aearost/aranarthcore/event/PotionConsume.java
+++ src/com/aearost/aranarthcore/event/PotionConsume.java
@@ -1,16 +1,22 @@
 package com.aearost.aranarthcore.event;
 
+import java.util.List;
 import java.util.Objects;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
 
 import com.aearost.aranarthcore.AranarthCore;
+import com.aearost.aranarthcore.objects.AranarthPlayer;
+import com.aearost.aranarthcore.utils.AranarthUtils;
 
 public class PotionConsume implements Listener {
 
@@ -41,7 +47,24 @@ public class PotionConsume implements Listener {
 			if (Objects.nonNull(e.getItem())) {
 				if (e.getItem().getType() == Material.SPLASH_POTION
 						|| e.getItem().getType() == Material.LINGERING_POTION) {
-					
+					Player player = e.getPlayer();
+					AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(player.getUniqueId());
+					ItemStack thrownPotion = e.getItem();
+					PotionMeta thrownPotionMeta = (PotionMeta) thrownPotion.getItemMeta();
+					List<ItemStack> potions = aranarthPlayer.getPotions();
+					for (ItemStack potion : potions) {
+						PotionMeta potionMeta = (PotionMeta) potion.getItemMeta();
+						if (thrownPotion.getType() == potion.getType()) {
+							if (thrownPotionMeta.getBasePotionType() == potionMeta.getBasePotionType()) {
+								
+								
+								
+								
+								
+								return; // Must prevent other quantities from being consumed
+							}
+						}
+					}
 				}
 			}
 		}
