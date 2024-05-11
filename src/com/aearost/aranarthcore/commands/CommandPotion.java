71d6edb37f1da9253f30b33dbdeefc0a81d4f81d
BuildTools <unconfigured@null.spigotmc.org>
2024-05-11 18:15:42 -0400

    Fixed potion name formatting, and correcting sorting to truly be alphabetical

--- src/com/aearost/aranarthcore/commands/CommandPotion.java
+++ src/com/aearost/aranarthcore/commands/CommandPotion.java
@@ -1,9 +1,10 @@
 package com.aearost.aranarthcore.commands;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -16,12 +17,11 @@ import com.aearost.aranarthcore.gui.GuiPotions;
 import com.aearost.aranarthcore.objects.AranarthPlayer;
 import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
-import com.aearost.aranarthcore.utils.PotionEffectComparable;
 
 public class CommandPotion {
 
 	public static boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
+
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 
@@ -32,50 +32,44 @@ public class CommandPotion {
 			} else {
 				if (args[1].equals("view")) {
 					AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(player.getUniqueId());
-					
+
 					if (Objects.nonNull(aranarthPlayer.getPotions())) {
-						if (aranarthPlayer.getPotions().size() > 0) {
-							List<ItemStack> potions = aranarthPlayer.getPotions();
-							
-							// Sorts all potions alphabetically
-							PotionEffectComparable comparable = new PotionEffectComparable();
-							Collections.sort(potions, comparable);
-							
-							
-							
-							// Counts how many of each potion there is
-							HashMap<String, Integer> amountOfPotions = new HashMap<>();
-							for (ItemStack potionToCount : potions) {
-								PotionMeta meta = (PotionMeta) potionToCount.getItemMeta();
-								String potionName = ChatUtils.getFormattedItemName(meta.getBasePotionType().name());
-								System.out.println("Potion Name: " + potionName);
-								String finalizedName = addPotionConsumptionMethodToName(potionToCount, potionName);
-								System.out.println("Finalized Potion Name:" + finalizedName);
-								
-								if (amountOfPotions.containsKey(finalizedName)) {
-									Integer newAmount = Integer.valueOf(amountOfPotions.get(finalizedName).intValue() + 1);
-									amountOfPotions.put(finalizedName, newAmount);
-								} else {
-									amountOfPotions.put(finalizedName, 1);
-								}
+						List<ItemStack> potions = aranarthPlayer.getPotions();
+
+						// Counts how many of each potion there is
+						HashMap<String, Integer> amountOfPotions = new HashMap<>();
+						for (ItemStack potionToCount : potions) {
+							PotionMeta meta = (PotionMeta) potionToCount.getItemMeta();
+							String potionName = ChatUtils.getFormattedItemName(meta.getBasePotionType().name());
+							String finalizedName = addPotionConsumptionMethodToName(potionToCount, potionName);
+
+							if (amountOfPotions.containsKey(finalizedName)) {
+								Integer newAmount = Integer.valueOf(amountOfPotions.get(finalizedName).intValue() + 1);
+								amountOfPotions.put(finalizedName, newAmount);
+							} else {
+								amountOfPotions.put(finalizedName, 1);
 							}
-							
-							// Displays the potions
-							player.sendMessage(ChatUtils.chatMessage("&7Below are the potions you have stored:"));
-							
-							for (String potionName : amountOfPotions.keySet()) {
-								player.sendMessage(ChatUtils.chatMessage("&e" + potionName + " x" + amountOfPotions.get(potionName)));
-							}
-						} else {
-							player.sendMessage(ChatUtils.chatMessage("&7You don't have any stored potions!"));
 						}
+						
+						// Sorts all potion names alphabetically
+						SortedSet<String> sortedMap = new TreeSet<String>(amountOfPotions.keySet());
+
+						// Displays the potions
+						player.sendMessage(ChatUtils.chatMessage("&7Below are the potions you have stored:"));
+						// Iterate over sortedMap but display values from amountOfPotions
+						for (String potionName : sortedMap) {
+							player.sendMessage(
+									ChatUtils.chatMessage("&e" + potionName + " x" + amountOfPotions.get(potionName)));
+						}
+					} else {
+						player.sendMessage(ChatUtils.chatMessage("&7You don't have any stored potions!"));
 					}
-					
+
 				} else if (args[1].equals("add")) {
-					
+
 					GuiPotions gui = new GuiPotions(player);
 					gui.openGui();
-					
+
 				} else if (args[1].equals("remove")) {
 					// Check next sub-commands to ensure valid syntax
 					// Also will need auto-completer to display all potions that they have
@@ -91,7 +85,7 @@ public class CommandPotion {
 		}
 		return false;
 	}
-	
+
 	private static String addPotionConsumptionMethodToName(ItemStack potion, String potionName) {
 		String[] partsOfName = potionName.split(" ");
 		String finalName = "";
@@ -121,8 +115,18 @@ public class CommandPotion {
 			}
 		}
 		
-		for (int i = 1; i < partsOfName.length; i++) {
-			finalName += partsOfName[i];
+		// Handles formatting the actual potion name
+		for (int i = 0; i < partsOfName.length; i++) {
+			if (partsOfName[i].equals("Long") || partsOfName[0].equals("Strong")) {
+				continue;
+			} else {
+				if (i == partsOfName.length - 1) {
+					finalName += partsOfName[i];
+				} else {
+					finalName += partsOfName[i] + " ";
+				}
+			}
+			
 		}
 		return finalName;
 	}
--- src/com/aearost/aranarthcore/utils/PotionEffectComparator.java
+++ src/com/aearost/aranarthcore/utils/PotionEffectComparator.java
@@ -5,14 +5,15 @@ import java.util.Comparator;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 
-public class PotionEffectComparable implements Comparator<ItemStack> {
+public class PotionEffectComparator implements Comparator<ItemStack> {
 
 	@Override
 	public int compare(ItemStack potion1, ItemStack potion2) {
 		PotionMeta potion1Meta = (PotionMeta) potion1.getItemMeta();
 		PotionMeta potion2Meta = (PotionMeta) potion2.getItemMeta();
 		
-		return potion1Meta.getBasePotionType().name().compareTo(potion2Meta.getBasePotionType().name());
+		return potion1Meta.getBasePotionType().name().compareToIgnoreCase(
+				potion2Meta.getBasePotionType().name());
 	}
 
 }
