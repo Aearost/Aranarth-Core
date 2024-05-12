9747fe2ed642b7118be4b0d04edd32d81ca48abf
BuildTools <unconfigured@null.spigotmc.org>
2024-05-11 22:17:27 -0400

    Potions are now automatically refilled based on what is in the potion inventory

--- src/com/aearost/aranarthcore/event/PotionConsume.java
+++ src/com/aearost/aranarthcore/event/PotionConsume.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Objects;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,6 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 
@@ -32,31 +34,12 @@ public class PotionConsume implements Listener {
 	 */
 	@EventHandler
 	public void onPotionUse(final PlayerItemConsumeEvent e) {
-		Player player = e.getPlayer();
-		AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(player.getUniqueId());
-		
-		// Can remove duplicate code by putting call to method as it is the same in both
-		// Just test to make sure it works
-		
-		ItemStack consumedPotion = e.getItem();
-		PotionMeta consumedPotionMeta = (PotionMeta) consumedPotion.getItemMeta();
-		List<ItemStack> potions = aranarthPlayer.getPotions();
-		for (ItemStack potion : potions) {
-			PotionMeta potionMeta = (PotionMeta) potion.getItemMeta();
-			if (consumedPotion.getType() == potion.getType()) {
-				if (consumedPotionMeta.getBasePotionType() == potionMeta.getBasePotionType()) {
-					
-					// It depends if the potion is consumed at the time of the event or not
-					e.setCancelled(true);
-					// Might cause exception due to it being deleted even though it's being iterated
-					potions.remove(potion);
-					aranarthPlayer.setPotions(potions);
-					AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
-					
-					return; // Must prevent other quantities from being consumed
-				}
-			}
+		if (e.getHand() == EquipmentSlot.HAND) {
+			replacePotion(e.getPlayer(), e.getItem(), true);
+		} else {
+			replacePotion(e.getPlayer(), e.getItem(), false);
 		}
+		
 	}
 
 	/**
@@ -71,27 +54,45 @@ public class PotionConsume implements Listener {
 			if (Objects.nonNull(e.getItem())) {
 				if (e.getItem().getType() == Material.SPLASH_POTION
 						|| e.getItem().getType() == Material.LINGERING_POTION) {
-					Player player = e.getPlayer();
-					AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(player.getUniqueId());
-					ItemStack thrownPotion = e.getItem();
-					PotionMeta thrownPotionMeta = (PotionMeta) thrownPotion.getItemMeta();
-					List<ItemStack> potions = aranarthPlayer.getPotions();
-					for (ItemStack potion : potions) {
-						PotionMeta potionMeta = (PotionMeta) potion.getItemMeta();
-						if (thrownPotion.getType() == potion.getType()) {
-							if (thrownPotionMeta.getBasePotionType() == potionMeta.getBasePotionType()) {
-								
-								// It depends if the potion is consumed at the time of the event or not
-								e.setCancelled(true);
-								// Might cause exception due to it being deleted even though it's being iterated
-								potions.remove(potion);
-								aranarthPlayer.setPotions(potions);
-								AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
-								
-								return; // Must prevent other quantities from being consumed
-							}
-						}
+					if (e.getHand() == EquipmentSlot.HAND) {
+						replacePotion(e.getPlayer(), e.getItem(), true);
+					} else {
+						replacePotion(e.getPlayer(), e.getItem(), false);
 					}
+				}
+			}
+		}
+	}
+	
+	private void replacePotion(Player player, ItemStack consumedPotion, boolean isUsedFromMainHand) {
+		
+		if (player.getGameMode() != GameMode.SURVIVAL) {
+			return;
+		}
+		
+		AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(player.getUniqueId());
+		PotionMeta consumedPotionMeta = (PotionMeta) consumedPotion.getItemMeta();
+		
+		List<ItemStack> potions = aranarthPlayer.getPotions();
+		for (ItemStack potion : potions) {
+			PotionMeta potionMeta = (PotionMeta) potion.getItemMeta();
+			if (consumedPotion.getType() == potion.getType()) {
+				if (consumedPotionMeta.getBasePotionType() == potionMeta.getBasePotionType()) {
+					// This might not include potions thrown from off-hand
+					int slot = 0;
+					if (isUsedFromMainHand) {
+						slot = player.getInventory().getHeldItemSlot();
+					} else {
+						// This is the slot number for the off-hand
+						slot = 40;
+					}
+					
+					potion.setAmount(2);
+					player.getInventory().setItem(slot, potion);
+					potions.remove(potion);
+					aranarthPlayer.setPotions(potions);
+					AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
+					return;
 				}
 			}
 		}
