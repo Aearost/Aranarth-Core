802dc3dd9ddeb5640bbf16d8b6e8f94979e65e6e
BuildTools <unconfigured@null.spigotmc.org>
2024-05-13 17:32:31 -0400

    Ensures that quivers are in the player's inventory in order for the arrows to be replenished

--- src/com/aearost/aranarthcore/event/ArrowConsume.java
+++ src/com/aearost/aranarthcore/event/ArrowConsume.java
@@ -51,44 +51,58 @@ public class ArrowConsume implements Listener {
 			return;
 		}
 		AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(player.getUniqueId());
-
-		List<ItemStack> arrows = aranarthPlayer.getArrows();
-		if (Objects.nonNull(arrows)) {
-			for (ItemStack arrow : arrows) {
-				if (Objects.nonNull(arrow)) {
-					// If it is a normal or spectral arrow
-					if (launchedArrow.getType() == arrow.getType()) {
-						
-						if (launchedArrow.getType() == Material.ARROW) {
-							player.getInventory().addItem(new ItemStack(Material.ARROW, 1));
-						} else if (launchedArrow.getType() == Material.SPECTRAL_ARROW) {
-							player.getInventory().addItem(new ItemStack(Material.SPECTRAL_ARROW, 1));
-						} else {
-							PotionMeta launchedArrowMeta = (PotionMeta) launchedArrow.getItemMeta();
-							PotionMeta arrowMeta = (PotionMeta) arrow.getItemMeta();
-							if (launchedArrowMeta.getBasePotionType() != arrowMeta.getBasePotionType()) {
-								return;
+		
+		// Ensures that the user has a Quiver in their inventory
+		boolean hasQuiver = false;
+		for (ItemStack item : player.getInventory()) {
+			if (Objects.isNull(item)) {
+				continue;
+			}
+			if (item.getType() == Material.BUNDLE) {
+				if (item.hasItemMeta()) {
+					hasQuiver = true;
+				}
+			}
+		}
+		
+		if (hasQuiver) {
+			List<ItemStack> arrows = aranarthPlayer.getArrows();
+			if (Objects.nonNull(arrows)) {
+				for (ItemStack arrow : arrows) {
+					if (Objects.nonNull(arrow)) {
+						// If it is a normal or spectral arrow
+						if (launchedArrow.getType() == arrow.getType()) {
+							
+							if (launchedArrow.getType() == Material.ARROW) {
+								player.getInventory().addItem(new ItemStack(Material.ARROW, 1));
+							} else if (launchedArrow.getType() == Material.SPECTRAL_ARROW) {
+								player.getInventory().addItem(new ItemStack(Material.SPECTRAL_ARROW, 1));
 							} else {
-								ItemStack arrowToAdd = new ItemStack(Material.TIPPED_ARROW, 1);
-								arrowToAdd.setItemMeta(launchedArrowMeta);
-								player.getInventory().addItem(arrowToAdd);
+								PotionMeta launchedArrowMeta = (PotionMeta) launchedArrow.getItemMeta();
+								PotionMeta arrowMeta = (PotionMeta) arrow.getItemMeta();
+								if (launchedArrowMeta.getBasePotionType() != arrowMeta.getBasePotionType()) {
+									return;
+								} else {
+									ItemStack arrowToAdd = new ItemStack(Material.TIPPED_ARROW, 1);
+									arrowToAdd.setItemMeta(launchedArrowMeta);
+									player.getInventory().addItem(arrowToAdd);
+								}
 							}
+							
+							int newAmountInQuiver = arrow.getAmount() - 1;
+							if (newAmountInQuiver > 0) {
+								arrow.setAmount(newAmountInQuiver);
+							} else {
+								arrows.remove(arrow);
+							}
+							
+							aranarthPlayer.setArrows(arrows);
+							AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
+							return;
 						}
-						
-						int newAmountInQuiver = arrow.getAmount() - 1;
-						if (newAmountInQuiver > 0) {
-							arrow.setAmount(newAmountInQuiver);
-						} else {
-							arrows.remove(arrow);
-						}
-						
-						aranarthPlayer.setArrows(arrows);
-						AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
-						return;
 					}
 				}
 			}
 		}
 	}
-
 }
