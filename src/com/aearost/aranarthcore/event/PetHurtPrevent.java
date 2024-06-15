81abab5d3b05e879701fba8173db10eaffd6f532
BuildTools <unconfigured@null.spigotmc.org>
2024-06-15 09:06:13 -0400

    Removed redundant debugging print statements

--- src/com/aearost/aranarthcore/event/PetHurtPrevent.java
+++ src/com/aearost/aranarthcore/event/PetHurtPrevent.java
@@ -29,20 +29,14 @@ public class PetHurtPrevent implements Listener {
 	 */
 	@EventHandler
 	public void onPetHurt(final EntityDamageEvent e) {
-		System.out.println("A");
 		Entity entity = e.getEntity();
 		if (entity instanceof Tameable) {
-			System.out.println("B");
 			if (isPlayerCausedDamage(e.getCause())) {
-				System.out.println("C");
 				Tameable pet = (Tameable) entity;
 				if (Objects.nonNull(pet.getOwner())) {
-					System.out.println("D");
 					if (pet.getOwner() instanceof OfflinePlayer) {
-						System.out.println("E");
 						if (Objects.nonNull(e.getDamageSource().getCausingEntity())
 								&& e.getDamageSource().getCausingEntity() instanceof Player) {
-							System.out.println("F");
 							e.setCancelled(true);
 							OfflinePlayer owner = (OfflinePlayer) pet.getOwner();
 							Player attacker = (Player) e.getDamageSource().getCausingEntity();
