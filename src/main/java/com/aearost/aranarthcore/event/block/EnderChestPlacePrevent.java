df1120d92c8ec8a4f7bc6369599f9f0755efb8ce
BuildTools <unconfigured@null.spigotmc.org>
2024-09-08 07:00:24 -0400

    Made EnderChestPlacePrevent be based on the chest opening and not placing

--- src/main/java/com/aearost/aranarthcore/event/block/EnderChestPlacePrevent.java
+++ src/main/java/com/aearost/aranarthcore/event/block/EnderChestPlacePrevent.java
@@ -6,7 +6,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class EnderChestPlacePrevent implements Listener {
 
@@ -19,14 +21,16 @@ public class EnderChestPlacePrevent implements Listener {
 	 * @param e The event.
 	 */
 	@EventHandler
-	public void onEnderChestPlate(final BlockPlaceEvent e) {
-		if (e.getBlockPlaced().getType() == Material.ENDER_CHEST) {
-			if (e.getPlayer().getWorld().getName().equalsIgnoreCase("creative")) {
-				if (!e.getPlayer().isOp()) {
-					e.setCancelled(true);
-					e.getPlayer().sendMessage(ChatUtils.chatMessage("&cYou cannot place an enderchest in creative!"));
-					for (int i = 0; i < 100; i++) {
-						Bukkit.getLogger().info(e.getPlayer().getName() + " placed an enderchest in creative!");
+	public void onEnderChestPlate(final PlayerInteractEvent e) {
+		if (e.getHand() == EquipmentSlot.HAND && e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (e.getClickedBlock().getType() == Material.ENDER_CHEST) {
+				if (e.getPlayer().getWorld().getName().equalsIgnoreCase("creative")) {
+					if (!e.getPlayer().isOp()) {
+						e.setCancelled(true);
+						e.getPlayer().sendMessage(ChatUtils.chatMessage("&cYou cannot open an enderchest in creative!"));
+						for (int i = 0; i < 100; i++) {
+							Bukkit.getLogger().info(e.getPlayer().getName() + " tried to open an enderchest in creative!");
+						}
 					}
 				}
 			}
