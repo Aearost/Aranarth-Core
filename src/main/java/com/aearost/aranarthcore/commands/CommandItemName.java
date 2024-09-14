ec3e17192354dd5ab6f083983739d65e2b7f7b9a
aearost <liam.harbec13@gmail.com>
2024-09-14 16:55:57 -0400

    Added itemname command Added gradient color support

--- src/main/java/com/aearost/aranarthcore/commands/CommandItemName.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandItemName.java
@@ -1,14 +1,17 @@
 package com.aearost.aranarthcore.commands;
 
 import com.aearost.aranarthcore.utils.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Objects;
+
 /**
- * Allows a player to toggle the shulker-filling functionality
+ * Allows specified players to rename any item to any name, supporting all colors.
  */
 public class CommandItemName {
 
@@ -30,46 +33,63 @@ public class CommandItemName {
                 ItemStack item = player.getInventory().getItemInMainHand();
                 if (item.getType() == Material.AIR) {
                     player.sendMessage(ChatUtils.chatMessage("&cYou must be holding an item to use this command!"));
-                }
-
-                if (item.hasItemMeta()) {
+                } else {
+                    ItemMeta meta = item.getItemMeta();
                     if (args.length == 2) {
                         if (args[1].equalsIgnoreCase("remove")) {
-                            ItemMeta meta = item.getItemMeta();
                             meta.setDisplayName(null);
-                            item.setItemMeta(meta);
-                            player.getInventory().setItemInMainHand(item);
                             player.sendMessage(ChatUtils.chatMessage("&7You have removed the name from this item"));
-                            return true;
+                        } else {
+                            String itemName = ChatUtils.translateToColor(args[1]);
+                            meta.setDisplayName(itemName);
+                            player.sendMessage(ChatUtils.chatMessage("&7You have named this item " + itemName));
                         }
-                    } if (args.length > 2) {
-                        int stringStart = 1;
-                        if (args[1].equalsIgnoreCase("gradient")) {
-                            stringStart++;
-                        }
-
-                        // Gets the full item name
-                        StringBuilder itemName = new StringBuilder();
-                        for (int i = 1; i < args.length; i++) {
-                            itemName.append(args[i]);
-                            if (i == args.length - 1) {
-                                break;
-                            } else {
-                                itemName.append(" ");
-                            }
-                        }
-
-                        ItemMeta meta = item.getItemMeta();
-                        meta.setDisplayName(ChatUtils.translateToColor(itemName.toString()));
                         item.setItemMeta(meta);
                         player.getInventory().setItemInMainHand(item);
-                        player.sendMessage(ChatUtils.chatMessage("&7You have named this item " + itemName));
+
                         return true;
+                    } else {
+                        if (args.length > 2) {
+                            int stringStart = 1;
+                            if (args[1].equalsIgnoreCase("gradient")) {
+                                // Start at the actual name
+                                stringStart = 3;
+                            }
+
+                            // Gets the full item name
+                            StringBuilder itemNameSB = new StringBuilder();
+                            for (int i = stringStart; i < args.length; i++) {
+                                itemNameSB.append(args[i]);
+                                if (i == args.length - 1) {
+                                    break;
+                                } else {
+                                    itemNameSB.append(" ");
+                                }
+                            }
+
+                            String itemName = itemNameSB.toString();
+                            if (args[1].equalsIgnoreCase("gradient")) {
+                                itemName = ChatUtils.translateToGradient(args[2], itemName);
+                                if (Objects.isNull(itemName)) {
+                                    player.sendMessage(ChatUtils.chatMessage("&cYour item could not be renamed as a gradient"));
+                                    return false;
+                                } else {
+                                    Bukkit.getLogger().info("Value is: |" + itemName + "|");
+                                    meta.setDisplayName(itemName);
+                                }
+                            } else {
+                                Bukkit.getLogger().info("Value is 2: |" + itemName + "|");
+                                meta.setDisplayName(ChatUtils.translateToColor(itemName));
+                            }
+                            item.setItemMeta(meta);
+                            player.getInventory().setItemInMainHand(item);
+                            player.sendMessage(ChatUtils.chatMessage("&7You have named this item " + itemName));
+                            return true;
+                        }
                     }
                 }
             }
         }
         return false;
     }
-
 }
