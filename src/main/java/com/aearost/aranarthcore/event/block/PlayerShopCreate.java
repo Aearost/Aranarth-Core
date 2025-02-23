be938ab883cb3d9c0a9fe751146583c94b960a3e
aearost <liam.harbec13@gmail.com>
2025-02-22 22:28:13 -0500

    Restructured PlayerShopCreate and affected methods to be better divided with more methods

--- src/main/java/com/aearost/aranarthcore/event/block/PlayerShopCreate.java
+++ src/main/java/com/aearost/aranarthcore/event/block/PlayerShopCreate.java
@@ -7,6 +7,7 @@ import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Container;
 import org.bukkit.entity.Player;
@@ -28,113 +29,161 @@ public class PlayerShopCreate implements Listener {
 	}
 
 	/**
-	 * Handles the creation of a player shop.
+	 * Handles the creation or updating of a player shop.
 	 * @param e The event.
 	 */
 	@EventHandler
 	public void onPlayerShopCreate(final SignChangeEvent e) {
-		for (int i = 0; i < e.getLines().length; i++) {
+		String[] lines = e.getLines();
+		if (lines[0].equals(ChatUtils.stripColorFormatting("[Shop]"))) {
+			Player player = e.getPlayer();
+			Block sign = e.getBlock();
 
-//			[Shop]
-//			Aearost
-//			QTY 15
-//			Buy 10
+			// Verifies that the sign follows the shop format
+			if (isValidSignFormat(lines, player)) {
+				Bukkit.getLogger().info("Valid sign format");
 
-			if (e.getLines()[i].equals(ChatUtils.stripColorFormatting("[Shop]"))) {
-				// Editing an existing shop
-				if (AranarthUtils.isShop(e.getBlock().getLocation())) {
-					PlayerShop shop = AranarthUtils.getShop(e.getBlock().getLocation());
-					Player player = e.getPlayer();
-					if (player.getName().equals(e.getLines()[1])) {
-						String[] line3Parts = e.getLines()[2].split(" ");
-						if (line3Parts[0].equals("QTY")) {
-							int quantity = 0;
-							try {
-								quantity = Integer.parseInt(line3Parts[1]);
-								if (quantity <= 0) {
-									throw new NumberFormatException();
-								}
-							} catch (NumberFormatException ex) {
-								player.sendMessage(ChatUtils.chatMessage("&cThat is an invalid quantity!"));
-								clearLines(e);
-							}
+				// Verifies there is a sign on top, a chest underneath, and at least one item in the first slot of the chest
+				if (isValidChestFormat(player, sign)) {
+					Bukkit.getLogger().info("Valid chest format");
 
-							String[] line4Parts = e.getLines()[3].split(" ");
-							if (line4Parts[0].equals("Buy")) {
-								int buyPrice = 0;
-								try {
-									buyPrice = Integer.parseInt(line4Parts[1]);
-									if (buyPrice <= 0) {
-										throw new NumberFormatException();
-									}
-									DecimalFormat df = new DecimalFormat("0.00");
-									buyPrice = Integer.parseInt(df.format(buyPrice));
-								} catch (NumberFormatException ex) {
-									player.sendMessage(ChatUtils.chatMessage("&cThat is an invalid quantity!"));
-									clearLines(e);
-								}
-								Location signLocation = e.getBlock().getLocation();
-								Location blockBelowLocation = new Location(signLocation.getWorld(), signLocation.getBlockX(),
-										signLocation.getBlockY() - 1, signLocation.getBlockZ());
-								if (isBlockBelowChest(signLocation, blockBelowLocation)) {
-									// Gets the first item in the chest and uses this as the item
-									BlockState state = blockBelowLocation.getBlock().getState();
-									Container container = (Container) state;
-									ItemStack firstItem = container.getInventory().getContents()[0];
-									if (firstItem != null) {
-										Bukkit.getLogger().info("First slot is: " + firstItem.getType().name());
-										ItemStack shopItem = firstItem.clone();
-										shopItem.setAmount(1);
-
-										// If everything is good, create a new shop
-										PlayerShop newShop = new PlayerShop(player.getUniqueId(), blockBelowLocation, shopItem, quantity, buyPrice, 0);
-
-										HashMap<UUID, List<PlayerShop>> shops = AranarthUtils.getShops();
-										if (shops == null) {
-											shops = new HashMap<>();
-										}
-
-										List<PlayerShop> playerShops = shops.get(player.getUniqueId());
-										if (playerShops == null) {
-											playerShops = new ArrayList<>();
-										}
-
-										// Updating an existing shop
-										if (AranarthUtils.isShop(shop.getLocation())) {
-											int index = 0;
-											for (PlayerShop storedShop : playerShops) {
-												if (storedShop.getLocation().getBlockX() == shop.getLocation().getBlockX()
-													&& storedShop.getLocation().getBlockY() == shop.getLocation().getBlockY()
-													&& storedShop.getLocation().getBlockZ() == shop.getLocation().getBlockZ()) {
-													break;
-												}
-												index++;
-											}
-
-											playerShops.set(index, newShop);
-										} else {
-											playerShops.add(newShop);
-										}
-										e.setLine(0, ChatUtils.translateToColor("&6&l[Shop]"));
-										e.setLine(1, ChatUtils.translateToColor("&0&l" + player.getName()));
-										player.sendMessage(ChatUtils.chatMessage("&7You have created a new shop!"));
-									}
-								}
-							} else {
-								player.sendMessage(ChatUtils.chatMessage("&cInvalid syntax for Buy!"));
-								clearLines(e);
-							}
-
-						} else {
-							player.sendMessage(ChatUtils.chatMessage("&cInvalid syntax for QTY!"));
-							clearLines(e);
-						}
+					if (lines[3].startsWith("Buy")) {
+						createOrUpdateShop(sign, player, getShopItem(sign), getShopQuantity(lines[2]), getShopPrice(lines[3]), 0);
 					} else {
-						player.sendMessage(ChatUtils.chatMessage("This is not your username!"));
-						clearLines(e);
+						createOrUpdateShop(sign, player, getShopItem(sign), getShopQuantity(lines[2]), 0, getShopPrice(lines[3]));
 					}
+				} else {
+					clearLines(e);
 				}
+			} else {
+				clearLines(e);
 			}
+		}
+	}
+
+	private boolean isValidSignFormat(String[] lines, Player player) {
+		// [Shop]
+		// Aearost
+		// QTY 15
+		// Buy 10
+		if (!isUsernameMatching(lines[1], player)) {
+			player.sendMessage(ChatUtils.chatMessage("&cThat is not your username!"));
+			return false;
+		}
+
+		int shopQuantityResult = getShopQuantity(lines[2]);
+		if (shopQuantityResult == 0) {
+			player.sendMessage(ChatUtils.chatMessage("&cThat is an invalid quantity!"));
+			return false;
+		} else if (shopQuantityResult == -1) {
+			player.sendMessage(ChatUtils.chatMessage("&cIncorrect syntax for quantity!"));
+			return false;
+		}
+
+		if (lines[3].startsWith("Buy")) {
+			if (getShopPrice(lines[3]) == 0) {
+				player.sendMessage(ChatUtils.chatMessage("&cThat is an invalid buying price!"));
+				return false;
+			} else if (getShopPrice(lines[3]) == -1) {
+				player.sendMessage(ChatUtils.chatMessage("&cIncorrect syntax for buying price!"));
+				return false;
+			}
+		} else if (lines[3].startsWith("Sell")) {
+			if (getShopPrice(lines[3]) == 0) {
+				player.sendMessage(ChatUtils.chatMessage("&cThat is an invalid selling price!"));
+				return false;
+			} else if (getShopPrice(lines[3]) == -1) {
+				player.sendMessage(ChatUtils.chatMessage("&cIncorrect syntax for selling price!"));
+				return false;
+			}
+		} else {
+			player.sendMessage(ChatUtils.chatMessage("&cYou must either buy or sell here!"));
+			return false;
+		}
+		// Will confirm only if all checks are valid
+		return true;
+    }
+
+	private boolean isUsernameMatching(String line, Player player) {
+		return line.equals(player.getName());
+	}
+
+	private int getShopQuantity(String line) {
+		String[] quantityLineParts = line.split(" ");
+		if (ChatUtils.stripColorFormatting(quantityLineParts[0]).equals("QTY")) {
+			int quantity = 0;
+			try {
+				quantity = Integer.parseInt(quantityLineParts[1]);
+				if (quantity <= 0) {
+					throw new NumberFormatException();
+				}
+				return quantity;
+			} catch (NumberFormatException ex) {
+				return 0;
+			}
+		} else {
+			return -1;
+		}
+	}
+
+	private double getShopPrice(String line) {
+		String[] priceLineParts = line.split(" ");
+		if (priceLineParts.length != 2) {
+			return -1;
+		}
+
+		int price = 0;
+		try {
+			price = Integer.parseInt(priceLineParts[1]);
+			if (price <= 0) {
+				throw new NumberFormatException();
+			}
+			DecimalFormat df = new DecimalFormat("0.00");
+			return Integer.parseInt(df.format(price));
+		} catch (NumberFormatException ex) {
+			return 0;
+		}
+	}
+
+	private boolean isValidChestFormat(Player player, Block sign) {
+		if (!isBlockBelowChest(sign)) {
+			player.sendMessage(ChatUtils.chatMessage("&cYou do not have a chest for the shop!"));
+			return false;
+		}
+
+		if (getShopItem(sign) == null) {
+			player.sendMessage(ChatUtils.chatMessage("&cThe first slot of the chest must contain an item!"));
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean isBlockBelowChest(Block sign) {
+		Location signLocation = sign.getLocation();
+		Location blockBelowLocation = new Location(signLocation.getWorld(),
+				signLocation.getBlockX(), signLocation.getBlockY() - 1, signLocation.getBlockZ());
+
+		return signLocation.getWorld().getBlockAt(blockBelowLocation).getType() == Material.CHEST
+				|| signLocation.getWorld().getBlockAt(blockBelowLocation).getType() == Material.BARREL
+				|| signLocation.getWorld().getBlockAt(blockBelowLocation).getType() == Material.TRAPPED_CHEST;
+	}
+
+	private ItemStack getShopItem(Block sign) {
+		Location signLocation = sign.getLocation();
+		Location blockBelowSign = new Location(signLocation.getWorld(),
+				signLocation.getBlockX(), signLocation.getBlockY() - 1, signLocation.getBlockZ());
+
+		// Gets the first item in the chest and uses this as the item
+		BlockState state = blockBelowSign.getBlock().getState();
+		Container container = (Container) state;
+		ItemStack firstItem = container.getInventory().getContents()[0];
+		if (firstItem != null) {
+			ItemStack shopItem = firstItem.clone();
+			shopItem.setAmount(1);
+			return shopItem;
+		} else {
+			return null;
 		}
 	}
 
@@ -145,10 +194,47 @@ public class PlayerShopCreate implements Listener {
 		e.setLine(3, "");
 	}
 
-	private boolean isBlockBelowChest(Location signLocation, Location blockBelowLocation) {
-		return signLocation.getWorld().getBlockAt(blockBelowLocation).getType() == Material.CHEST
-				|| signLocation.getWorld().getBlockAt(blockBelowLocation).getType() == Material.BARREL
-				|| signLocation.getWorld().getBlockAt(blockBelowLocation).getType() == Material.TRAPPED_CHEST;
+	private void createOrUpdateShop(Block sign, Player player, ItemStack shopItem, int quantity, double buyPrice, double sellPrice) {
+		// If everything is good, create a new shop
+		PlayerShop newShop = new PlayerShop(player.getUniqueId(), sign.getLocation(), shopItem, quantity, buyPrice, sellPrice);
+
+		HashMap<UUID, List<PlayerShop>> shops = AranarthUtils.getShops();
+		if (shops == null) {
+			shops = new HashMap<>();
+		}
+
+		List<PlayerShop> playerShops = shops.get(player.getUniqueId());
+		if (playerShops == null) {
+			playerShops = new ArrayList<>();
+		}
+
+		PlayerShop existingShop = AranarthUtils.getShop(player.getUniqueId(), sign.getLocation());
+
+		// Creating a new shop
+		if (existingShop == null) {
+			Bukkit.getLogger().info("Will create a new shop!");
+		}
+		// Updating an existing shop
+		else {
+//			if (AranarthUtils.isShop(shop.getLocation())) {
+//				int index = 0;
+//				for (PlayerShop storedShop : playerShops) {
+//					if (storedShop.getLocation().getBlockX() == shop.getLocation().getBlockX()
+//							&& storedShop.getLocation().getBlockY() == shop.getLocation().getBlockY()
+//							&& storedShop.getLocation().getBlockZ() == shop.getLocation().getBlockZ()) {
+//						break;
+//					}
+//					index++;
+//				}
+//
+//				playerShops.set(index, newShop);
+//			} else {
+//				playerShops.add(newShop);
+//			}
+//			e.setLine(0, ChatUtils.translateToColor("&6&l[Shop]"));
+//			e.setLine(1, ChatUtils.translateToColor("&0&l" + player.getName()));
+//			player.sendMessage(ChatUtils.chatMessage("&7You have created a new shop!"));
+		}
 	}
 
 }
