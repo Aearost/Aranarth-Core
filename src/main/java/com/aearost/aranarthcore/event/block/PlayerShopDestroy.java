290738fb93cc517d1088e67c176d8ec45bf00524
aearost <liam.harbec13@gmail.com>
2025-02-23 00:12:09 -0500

    Added support to add and remove shops Will need to correct how they persist, and then add functionality to the interactions

--- src/main/java/com/aearost/aranarthcore/event/block/PlayerShopDestroy.java
+++ src/main/java/com/aearost/aranarthcore/event/block/PlayerShopDestroy.java
@@ -7,6 +7,10 @@ import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Sign;
+import org.bukkit.block.sign.Side;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -23,39 +27,43 @@ public class PlayerShopDestroy implements Listener {
 	 */
 	@EventHandler
 	public void onPlayerShopDestroy(final BlockBreakEvent e) {
+		Player player = e.getPlayer();
+		int deletionResult = -1;
+		Location destroyedSignLocation = null;
+		Location destroyedChestLocation = null;
+		// Must add support to check if sign is broken by block it is connected to
 		if (isSign(e.getBlock().getType())) {
-			Location destroyedSignLocation = e.getBlock().getLocation();
-			Location blockLocationBelow = new Location(destroyedSignLocation.getWorld(),
-					destroyedSignLocation.getBlockX(),
-					destroyedSignLocation.getBlockY() - 1,
-					destroyedSignLocation.getBlockZ());
-			if (isChestBlock(blockLocationBelow.getBlock().getType())) {
-				if (AranarthUtils.isShop(blockLocationBelow)) {
-					PlayerShop playerShop = AranarthUtils.getShop(e.getPlayer().getUniqueId(), blockLocationBelow);
-					if (e.getPlayer().getUniqueId() == playerShop.getUuid()) {
-						AranarthUtils.removeShop(e.getPlayer().getUniqueId(), blockLocationBelow);
-					} else {
-						e.getPlayer().sendMessage(ChatUtils.chatMessage("&cYou cannot destroy someone else's shop!"));
-					}
-				}
-			}
+			destroyedSignLocation = e.getBlock().getLocation();
+			deletionResult = deleteShopIfPossible(player, destroyedSignLocation);
 		}
 		else if (isChestBlock(e.getBlock().getType())) {
-			Location destroyedChestLocation = e.getBlock().getLocation();
-			Location blockLocationAbove = new Location(destroyedChestLocation.getWorld(),
+			destroyedChestLocation = e.getBlock().getLocation();
+			destroyedSignLocation = new Location(destroyedChestLocation.getWorld(),
 					destroyedChestLocation.getBlockX(),
 					destroyedChestLocation.getBlockY() + 1,
 					destroyedChestLocation.getBlockZ());
-			if (isSign(blockLocationAbove.getBlock().getType())) {
-				if (AranarthUtils.isShop(destroyedChestLocation)) {
-					PlayerShop playerShop = AranarthUtils.getShop(e.getPlayer().getUniqueId(), destroyedChestLocation);
-					if (e.getPlayer().getUniqueId() == playerShop.getUuid()) {
-						AranarthUtils.removeShop(e.getPlayer().getUniqueId(), destroyedChestLocation);
-					} else {
-						e.getPlayer().sendMessage(ChatUtils.chatMessage("&cYou cannot destroy someone else's shop!"));
-					}
-				}
+			if (isSign(destroyedSignLocation.getBlock().getType())) {
+				deletionResult = deleteShopIfPossible(player, destroyedSignLocation);
 			}
+		} else {
+			return;
+		}
+
+		if (deletionResult == 1) {
+			player.sendMessage(ChatUtils.chatMessage("&7You have destroyed this shop"));
+			player.playSound(player, Sound.BLOCK_NOTE_BLOCK_XYLOPHONE, 1F, 0.1F);
+			// If the chest was destroyed
+			if (destroyedChestLocation != null) {
+				Sign sign = (Sign) destroyedSignLocation.getBlock().getState();
+				sign.getSide(Side.FRONT).setLine(0, ChatUtils.translateToColor("&4&l[Shop]"));
+				sign.getSide(Side.FRONT).setLine(1, "");
+				sign.getSide(Side.FRONT).setLine(2, "");
+				sign.getSide(Side.FRONT).setLine(3, "");
+				sign.update();
+			}
+		} else if (deletionResult == 0){
+			player.sendMessage(ChatUtils.chatMessage("&cYou cannot destroy someone else's shop!"));
+			e.setCancelled(true);
 		}
 	}
 
@@ -65,6 +73,22 @@ public class PlayerShopDestroy implements Listener {
 
 	private boolean isChestBlock(Material type) {
 		return type == Material.CHEST || type == Material.TRAPPED_CHEST || type == Material.BARREL;
+	}
+
+	private int deleteShopIfPossible(Player player, Location destroyedSignLocation) {
+		if (AranarthUtils.isShop(destroyedSignLocation)) {
+			PlayerShop shop = AranarthUtils.getShop(player.getUniqueId(), destroyedSignLocation);
+			// If it is the player's shop
+			if (shop != null) {
+				AranarthUtils.removeShop(player.getUniqueId(), destroyedSignLocation);
+				return 1;
+			}
+			// If it is another player's shop
+			else {
+				return 0;
+			}
+		}
+		return -1;
 	}
 
 }
