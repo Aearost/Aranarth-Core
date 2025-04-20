c9a11d8270376358f35f055b4a8a4c7ea91db931
aearost <liam.harbec13@gmail.com>
2025-04-20 18:25:30 -0400

    Added message for god apple fragment being dropped to players within 32 blocks

--- src/main/java/com/aearost/aranarthcore/event/block/LeafDecayAppleDropIncrease.java
+++ src/main/java/com/aearost/aranarthcore/event/block/LeafDecayAppleDropIncrease.java
@@ -3,9 +3,11 @@ package com.aearost.aranarthcore.event.block;
 import com.aearost.aranarthcore.AranarthCore;
 import com.aearost.aranarthcore.items.GodAppleFragment;
 import com.aearost.aranarthcore.utils.AranarthUtils;
+import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.LeavesDecayEvent;
@@ -31,13 +33,17 @@ public class LeafDecayAppleDropIncrease implements Listener {
 			if (block.getType() == Material.OAK_LEAVES || block.getType() == Material.DARK_OAK_LEAVES) {
 				// 5% chance of dropping an apple instead of 0.5%
 				if (new Random().nextInt(20) == 0) {
-					Bukkit.getLogger().info("Apple");
 					block.getLocation().getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.APPLE));
 				}
-				// 1% chance of dropping a god apple fragment
-				else if (new Random().nextInt(100) == 0) {
-					Bukkit.getLogger().info("GOD Apple");
+				// 0.05% chance of dropping a god apple fragment
+				else if (new Random().nextInt(200) == 0) {
 					block.getLocation().getWorld().dropItemNaturally(block.getLocation(), GodAppleFragment.getGodAppleFragment());
+					for (Player player : Bukkit.getOnlinePlayers()) {
+						// If the player is within 32 blocks of the spawn location
+						if (block.getLocation().distance(player.getLocation()) <= 32) {
+							player.sendMessage(ChatUtils.chatMessage("&7A god apple fragment has been dropped!"));
+						}
+					}
 				}
 			}
 		}
