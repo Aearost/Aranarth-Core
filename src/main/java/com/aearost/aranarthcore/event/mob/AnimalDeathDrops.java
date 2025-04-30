d294715b7b76c2cf5c1c955f97320956768235e6
aearost <liam.harbec13@gmail.com>
2025-04-30 13:30:50 -0400

    Added increased animal drops during the month of Faunivor

---
+++ src/main/java/com/aearost/aranarthcore/event/mob/AnimalDeathDrops.java
@@ -0,0 +1,41 @@
+package com.aearost.aranarthcore.event.mob;
+
+import com.aearost.aranarthcore.AranarthCore;
+import com.aearost.aranarthcore.enums.Month;
+import com.aearost.aranarthcore.utils.AranarthUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
+
+public class AnimalDeathDrops implements Listener {
+
+	public AnimalDeathDrops(AranarthCore plugin) {
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+	
+	/**
+	 * Deals with dropping extra drops during the month of Faunivor.
+	 * @param e The event.
+	 */
+	@EventHandler
+	public void onAnimalDeath(final EntityDeathEvent e) {
+		if (AranarthUtils.getMonth() == Month.FAUNIVOR) {
+			for (ItemStack drop : e.getDrops()) {
+				int rand = new Random().nextInt(4);
+				// 50% chance to increase the drop by 1
+				if (rand <= 1) {
+					drop.setAmount(drop.getAmount() + 1);
+				}
+				// 25% chance to increase the drop by 2
+				else if (rand == 3) {
+					drop.setAmount(drop.getAmount() + 2);
+				}
+			}
+		}
+	}
+
+}
--- src/main/java/com/aearost/aranarthcore/event/mob/GoatDeath.java
+++ src/main/java/com/aearost/aranarthcore/event/mob/GoatDeath.java
@@ -1,6 +1,8 @@
 package com.aearost.aranarthcore.event.mob;
 
 import com.aearost.aranarthcore.AranarthCore;
+import com.aearost.aranarthcore.enums.Month;
+import com.aearost.aranarthcore.utils.AranarthUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
@@ -26,6 +28,10 @@ public class GoatDeath implements Listener {
 		if (e.getEntityType() == EntityType.GOAT) {
 			Random r = new Random();
 			int dropCount = r.nextInt(3) + 1;
+			if (AranarthUtils.getMonth() == Month.FAUNIVOR) {
+				dropCount = dropCount + 1;
+			}
+
 			if (e.getEntity().isVisualFire()) {
 				e.getEntity().getLocation().getWorld().dropItemNaturally(
 						e.getEntity().getLocation(), new ItemStack(Material.COOKED_MUTTON, dropCount));
