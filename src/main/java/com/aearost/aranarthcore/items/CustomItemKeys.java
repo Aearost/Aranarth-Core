40ef85679b1bed23b9d72d343c85eee0986a82dc
aearost <liam.harbec13@gmail.com>
2025-06-08 11:48:00 -0400

    Added persistent data for Aquatic armour set Also added defined potion effects for each set of armour Will aadd persistent data for all other armour sets

---
+++ src/main/java/com/aearost/aranarthcore/items/CustomItemKeys.java
@@ -0,0 +1,8 @@
+package com.aearost.aranarthcore.items;
+
+import com.aearost.aranarthcore.AranarthCore;
+import org.bukkit.NamespacedKey;
+
+public class CustomItemKeys {
+    public static final NamespacedKey ARMOUR_TYPE = new NamespacedKey(AranarthCore.getInstance(), "armour_type");
+}
--- src/main/java/com/aearost/aranarthcore/items/aranarthium/armour/AquaticAranarthiumHelmet.java
+++ src/main/java/com/aearost/aranarthcore/items/aranarthium/armour/AquaticAranarthiumHelmet.java
@@ -3,14 +3,18 @@ package com.aearost.aranarthcore.items.aranarthium.armour;
 import com.aearost.aranarthcore.items.AranarthItem;
 import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ArmorMeta;
 import org.bukkit.inventory.meta.trim.ArmorTrim;
 import org.bukkit.inventory.meta.trim.TrimMaterial;
 import org.bukkit.inventory.meta.trim.TrimPattern;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
 import java.util.Objects;
+
+import static com.aearost.aranarthcore.items.CustomItemKeys.ARMOUR_TYPE;
 
 /**
  * Provides the necessary components of an Aquatic Aranarthium Helmet item.
@@ -24,7 +28,9 @@ public class AquaticAranarthiumHelmet implements AranarthItem {
 		ItemStack item = new ItemStack(Material.NETHERITE_HELMET, 1);
 		ArmorMeta meta = (ArmorMeta) item.getItemMeta();
 		meta.setTrim(new ArmorTrim(TrimMaterial.DIAMOND, TrimPattern.COAST));
+		meta.addEnchant(Enchantment.AQUA_AFFINITY, 0, false);
 		ArrayList<String> lore = new ArrayList<>();
+		meta.getPersistentDataContainer().set(ARMOUR_TYPE, PersistentDataType.STRING, "aquatic");
 
 		if (Objects.nonNull(meta)) {
 			meta.setDisplayName(ChatUtils.translateToColor(getName()));
