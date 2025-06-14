1996bfc9c3e4957edef1f7d3069e1d7d417ae6f5
aearost <liam.harbec13@gmail.com>
2025-06-14 16:46:42 -0400

    Corrected crafting overrides for sugarcane blocks

--- src/main/java/com/aearost/aranarthcore/event/crafting/CraftingOverridesSugarcaneBlock.java
+++ src/main/java/com/aearost/aranarthcore/event/crafting/CraftingOverridesSugarcaneBlock.java
@@ -1,12 +1,15 @@
 package com.aearost.aranarthcore.event.crafting;
 
-import com.aearost.aranarthcore.items.SugarcaneBlock;
 import com.aearost.aranarthcore.utils.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import static com.aearost.aranarthcore.items.CustomItemKeys.*;
 
 /**
  * Handles the overrides when crafting involving Sugarcane Blocks.
@@ -22,9 +25,11 @@ public class CraftingOverridesSugarcaneBlock {
                 nullCounter++;
                 continue;
             }
-
-            if (is.isSimilar(new SugarcaneBlock().getItem())) {
-                sugarcaneBlockCounter++;
+            if (is.hasItemMeta()) {
+                ItemMeta meta = is.getItemMeta();
+                if (meta.getPersistentDataContainer().has(SUGARCANE_BLOCK)) {
+                    sugarcaneBlockCounter++;
+                }
             }
         }
 
@@ -34,25 +39,21 @@ public class CraftingOverridesSugarcaneBlock {
     }
 
     public void onCraft(CraftItemEvent e, ItemStack is, HumanEntity player) {
-        boolean isHasLore = is.getItemMeta().hasLore();
-
-        // If a Sugarcane Block is used in place of a regular Sugarcane or Bamboo
-        if (isHasLore) {
-            if (e.getRecipe().getResult().getType() != Material.SUGAR_CANE &&
-                    e.getRecipe().getResult().getType() != Material.BAMBOO) {
-                e.setCancelled(true);
-                player.sendMessage(ChatUtils.chatMessage("&cYou cannot use a Sugarcane Block to craft this!"));
-                return;
-            }
+        if (is.getType() == Material.SUGAR_CANE) {
+            return;
         }
-        // If a Sugarcane or Bamboo is used in place of a Sugarcane Block
-        else {
-            if (e.getRecipe().getResult().getType() == Material.SUGAR_CANE) {
-                e.setCancelled(true);
-                player.sendMessage(ChatUtils.chatMessage("&cYou must use a Sugarcane Block to craft this!"));
-                return;
+
+        ItemStack result = e.getRecipe().getResult();
+        if (is.hasItemMeta()) {
+            ItemMeta meta = is.getItemMeta();
+            if (meta.getPersistentDataContainer().has(SUGARCANE_BLOCK)) {
+                // preCraft does not actually update it to sugarcane in the backend
+                if (result.getType() != Material.BAMBOO) {
+                    player.sendMessage(ChatUtils.chatMessage("&cYou cannot use a Block of Sugarcane to craft this!"));
+                    Bukkit.getLogger().info(result.getType().name());
+                    e.setCancelled(true);
+                }
             }
         }
     }
-
 }
