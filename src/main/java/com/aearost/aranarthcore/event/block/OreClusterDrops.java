86f432bf0c3c7184d3f43970adb80250c7132b23
aearost <liam.harbec13@gmail.com>
2025-06-21 20:08:01 -0400

    Added fortune drop bonus for ore clusters Adjusted the cluster drop rates Also renamed ore cluster drops

--- src/main/java/com/aearost/aranarthcore/event/block/OreClusterDrops.java
+++ src/main/java/com/aearost/aranarthcore/event/block/OreClusterDrops.java
@@ -11,9 +11,9 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Random;
 
 /**
- * Adds functionality of ore fragment and clusters when breaking ore blocks.
+ * Adds functionality of ore clusters when breaking ore blocks.
  */
-public class OreFragmentDrops {
+public class OreClusterDrops {
 	public void execute(BlockBreakEvent e) {
 		Material material = e.getBlock().getType();
 		Random random = new Random();
@@ -23,42 +23,55 @@ public class OreFragmentDrops {
 		if (loc.getWorld().getName().startsWith("world")) {
 			ItemStack heldItem = e.getPlayer().getInventory().getItemInMainHand();
 			if (!heldItem.containsEnchantment(Enchantment.SILK_TOUCH)) {
+				// Increased drop rate with fortune
+				double fortuneBonus = 1;
+				if (heldItem.containsEnchantment(Enchantment.FORTUNE)) {
+					int enchantmentLevel = heldItem.getEnchantmentLevel(Enchantment.FORTUNE);
+					if (enchantmentLevel == 1) {
+						fortuneBonus = 0.9;
+					} else if (enchantmentLevel == 2) {
+						fortuneBonus = 0.7;
+					} else if (enchantmentLevel == 3) {
+						fortuneBonus = 0.5;
+					}
+				}
+
 				if (material == Material.DIAMOND_ORE || material == Material.DEEPSLATE_DIAMOND_ORE) {
-					if (random.nextInt(125) == 0) {
+					if (random.nextInt((int) (30 * fortuneBonus)) == 0) {
 						world.dropItemNaturally(loc, new DiamondCluster().getItem());
 					}
 				} else if (material == Material.EMERALD_ORE || material == Material.DEEPSLATE_EMERALD_ORE) {
-					if (random.nextInt(100) == 0) {
+					if (random.nextInt((int) (20 * fortuneBonus)) == 0) {
 						world.dropItemNaturally(loc, new EmeraldCluster().getItem());
 					}
-				} else if (material.name().endsWith("_GOLD_ORE")) {
+				} else if (material.name().endsWith("GOLD_ORE")) {
 					if (material == Material.NETHER_GOLD_ORE) {
-						if (random.nextInt(750) == 0) {
+						if (random.nextInt((int) (200 * fortuneBonus)) == 0) {
 							world.dropItemNaturally(loc, new GoldCluster().getItem());
 						}
 					} else {
-						if (random.nextInt(250) == 0) {
+						if (random.nextInt((int) (65 * fortuneBonus)) == 0) {
 							world.dropItemNaturally(loc, new GoldCluster().getItem());
 						}
 					}
 				} else if (material == Material.IRON_ORE || material == Material.DEEPSLATE_IRON_ORE) {
-					if (random.nextInt(350) == 0) {
+					if (random.nextInt((int) (80 * fortuneBonus)) == 0) {
 						world.dropItemNaturally(loc, new IronCluster().getItem());
 					}
 				} else if (material == Material.COPPER_ORE || material == Material.DEEPSLATE_COPPER_ORE) {
-					if (random.nextInt(550) == 0) {
+					if (random.nextInt((int) (200 * fortuneBonus)) == 0) {
 						world.dropItemNaturally(loc, new CopperCluster().getItem());
 					}
 				} else if (material == Material.REDSTONE_ORE || material == Material.DEEPSLATE_REDSTONE_ORE) {
-					if (random.nextInt(300) == 0) {
+					if (random.nextInt((int) (75 * fortuneBonus)) == 0) {
 						world.dropItemNaturally(loc, new RedstoneCluster().getItem());
 					}
 				} else if (material == Material.LAPIS_ORE || material == Material.DEEPSLATE_LAPIS_ORE) {
-					if (random.nextInt(200) == 0) {
+					if (random.nextInt((int) (45 * fortuneBonus)) == 0) {
 						world.dropItemNaturally(loc, new LapisCluster().getItem());
 					}
 				} else if (material == Material.NETHER_QUARTZ_ORE) {
-					if (random.nextInt(750) == 0) {
+					if (random.nextInt((int) (175 * fortuneBonus)) == 0) {
 						world.dropItemNaturally(loc, new QuartzCluster().getItem());
 					}
 				}
