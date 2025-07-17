8af11c2516c9939ca4ded5e229bbd961396bc977
aearost <liam.harbec13@gmail.com>
2025-07-17 17:32:14 -0400

    Removed redundant comments

--- src/main/java/com/aearost/aranarthcore/event/listener/misc/PlayerTeleportBetweenWorldsListener.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/misc/PlayerTeleportBetweenWorldsListener.java
@@ -30,20 +30,16 @@ public class PlayerTeleportBetweenWorldsListener implements Listener {
 			Player player = e.getPlayer();
 			String currentWorld = e.getFrom().getWorld().getName();
 			String destinationWorld = e.getTo().getWorld().getName();
-			Bukkit.getLogger().info("A");
 			if (!currentWorld.equals(destinationWorld)) {
-				Bukkit.getLogger().info("B");
 				GameMode newMode = GameMode.SURVIVAL;
 				if (destinationWorld.equalsIgnoreCase("creative")) {
 					newMode = GameMode.CREATIVE;
 				}
 
 				try {
-					Bukkit.getLogger().info("C");
 					AranarthUtils.switchInventory(player, currentWorld, destinationWorld);
 					player.setGameMode(newMode);
 					for (PotionEffect effect : player.getActivePotionEffects()) {
-						Bukkit.getLogger().info("D");
 						player.removePotionEffect(effect.getType());
 					}
 				} catch (IOException exception) {
@@ -51,12 +47,9 @@ public class PlayerTeleportBetweenWorldsListener implements Listener {
 					e.setCancelled(true);
 				}
 			} else {
-				Bukkit.getLogger().info("E");
 				try {
-					Bukkit.getLogger().info("F");
 					AranarthUtils.switchInventory(player, currentWorld, currentWorld);
 				} catch (IOException exception) {
-					Bukkit.getLogger().info("G");
 					player.sendMessage(ChatUtils.chatMessage("&cSomething went wrong with changing world."));
 					e.setCancelled(true);
 				}
