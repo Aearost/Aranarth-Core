77216db746e8e00453087c8d6f30baedfa308c45
aearost <liam.harbec13@gmail.com>
2025-07-26 14:51:20 -0400

    Fixed issues with pitcher plant and torchflowers not removing light block when destroying the block underneath them

--- src/main/java/com/aearost/aranarthcore/event/block/TorchflowerBreak.java
+++ src/main/java/com/aearost/aranarthcore/event/block/TorchflowerBreak.java
@@ -12,10 +12,13 @@ public class TorchflowerBreak {
 	public void execute(BlockBreakEvent e) {
 		Location location = e.getBlock().getLocation();
 		Location locationAbove = new Location(location.getWorld(), location.getX(), location.getY() + 1, location.getZ());
-		
+
+		// Breaking the flower
 		if (location.getBlock().getType() == Material.TORCHFLOWER && locationAbove.getBlock().getType() == Material.LIGHT) {
 			locationAbove.getBlock().setType(Material.AIR);
-		} else if (locationAbove.getBlock().getType() == Material.TORCHFLOWER) {
+		}
+		// Breaking the block under the flower
+		else if (locationAbove.getBlock().getType() == Material.TORCHFLOWER) {
 			Location locationAboveTorchflower = new Location(locationAbove.getWorld(), locationAbove.getX(), locationAbove.getY() + 1, locationAbove.getZ());
 			locationAboveTorchflower.getBlock().setType(Material.AIR);
 		}
