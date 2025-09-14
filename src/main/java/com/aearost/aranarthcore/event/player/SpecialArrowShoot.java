1adec280c5181f28d5a4065eb1a993ef4e3e63f1
aearost <liam.harbec13@gmail.com>
2025-09-14 18:24:54 -0400

    Minor updates to javadocs

--- src/main/java/com/aearost/aranarthcore/event/player/SpecialArrowShoot.java
+++ src/main/java/com/aearost/aranarthcore/event/player/SpecialArrowShoot.java
@@ -1,6 +1,5 @@
 package com.aearost.aranarthcore.event.player;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.ItemStack;
@@ -9,7 +8,7 @@ import org.bukkit.persistence.PersistentDataType;
 import static com.aearost.aranarthcore.items.CustomItemKeys.ARROW;
 
 /**
- * Handles the auto-refill functionality when consuming of arrows.
+ * Handles adding the Persistent Data attributes to the custom special arrows.
  */
 public class SpecialArrowShoot {
 
@@ -17,11 +16,8 @@ public class SpecialArrowShoot {
         ItemStack arrowItem = e.getConsumable();
 
         // Apply the value to the projectile entity
-        Bukkit.getLogger().info("A");
         if (arrowItem.hasItemMeta()) {
-            Bukkit.getLogger().info("B");
             if (arrowItem.getItemMeta().getPersistentDataContainer().has(ARROW)) {
-                Bukkit.getLogger().info("C");
                 String type = arrowItem.getItemMeta().getPersistentDataContainer().get(ARROW, PersistentDataType.STRING);
                 Entity projectile = e.getProjectile();
                 projectile.getPersistentDataContainer().set(ARROW, PersistentDataType.STRING, type);
