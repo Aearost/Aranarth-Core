dcfde13de7f2ec05ab34fa6782a74e1729b7d3c0
aearost <liam.harbec13@gmail.com>
2025-10-07 19:43:07 -0400

    Can now create EXP bottles by right clicking blocks Also made EXP bottles get created based on current EXP but less than what they will reward

--- src/main/java/com/aearost/aranarthcore/event/player/ExpBottle.java
+++ src/main/java/com/aearost/aranarthcore/event/player/ExpBottle.java
@@ -7,6 +7,8 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Random;
+
 /**
  * Handles storing EXP in the player's held empty bottle.
  */
@@ -18,12 +20,16 @@ public class ExpBottle {
             if (player.getInventory().getItemInMainHand().getType() == Material.GLASS_BOTTLE) {
                 if (player.getLevel() > 0) {
                     if (player.isSneaking()) {
-                        player.getInventory().addItem(new ItemStack(Material.EXPERIENCE_BOTTLE));
-                        player.setLevel(player.getLevel() - 1);
-                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+                        // Reduces by a less than what is gained
+                        int amountToReduce = new Random().nextInt(10) + 10;
+                        amountToReduce = amountToReduce * -1;
+                        player.giveExp(amountToReduce);
+
                         player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1F, 1F);
                         player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1F, 1.5F);
                         player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1F, 2F);
+                        player.getInventory().addItem(new ItemStack(Material.EXPERIENCE_BOTTLE));
+                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
                     }
                 }
             }
