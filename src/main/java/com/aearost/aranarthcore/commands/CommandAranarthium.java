28ea0dbcaf1c5363f024377fb6bc3eb164c6d887
aearost <liam.harbec13@gmail.com>
2025-10-12 17:07:03 -0400

    Finished writing /ac aranarthium book

--- src/main/java/com/aearost/aranarthcore/commands/CommandAranarthium.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandAranarthium.java
@@ -21,11 +21,17 @@ public class CommandAranarthium {
 		if (sender instanceof Player player) {
 			ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
 			BookMeta meta = (BookMeta) book.getItemMeta();
-			meta.setItemName(ChatUtils.translateToColor("&8&l--=&6&lThe Realm of Aranarth&8&l=--"));
+			meta.setItemName(ChatUtils.translateToColor("&8&l--=&6&lAranarthium&8&l=--"));
 			meta.setAuthor(ChatUtils.translateToColor("&8Unknown"));
 
-			meta.addPage(bookIntroduction());
-
+			meta.addPage(bookIntro());
+			meta.addPage(ingotIntro());
+			meta.addPage(aquaticArmour());
+			meta.addPage(ardentArmour());
+			meta.addPage(dwarvenArmour());
+			meta.addPage(elvenArmour());
+			meta.addPage(scorchedArmour());
+			meta.addPage(soulboundArmour());
 
 			book.setItemMeta(meta);
 			player.getInventory().addItem(book);
@@ -36,12 +42,60 @@ public class CommandAranarthium {
 		}
 	}
 
-	private static String bookIntroduction() {
+	private static String bookIntro() {
 		return ChatUtils.translateToColor(
-				"Welcome to the &lRealm of Aranarth&r, where time, seasons, and the world is different than what we typically know." +
-				"\n\nIn this guide, you will find some key information on how to navigate the beautiful world, as well as all of the wonders that it has to offer."
+				"&OThe &lRealm of Aranarth&r&O exudes a sacred energy, infusing its ores with a little bit of magic.\n\n" +
+						"&l&nOre Clusters\n&rOre clusters are dropped from copper, iron, gold, nether quartz, lapis, redstone, diamond, and emerald ores, at very low rates."
 		);
 	}
 
+	private static String ingotIntro() {
+		return ChatUtils.translateToColor(
+				"&l&nAranarthium\n&rWhen crafting all 8 ore clusters together with a netherite ingot, an &lAranarthium Ingot&r is created.\n\n" +
+						"This ingot is used as a base to crafting all &oEnhanced Aranarthium Ingots&r, which are applied to netherite armour and grant special abilities."
+		);
+	}
+
+	private static String aquaticArmour() {
+		return ChatUtils.translateToColor(
+				"&lAquatic Aranarthium\n\n&r" +
+						"Provides the wearer with the status effects of &oDolphin's Grace&r and the &oConduit Power&r.\n\nAdditionally yields increased damage when using a &lTrident&r, and results in immunity of &oMiner's Fatigue."
+		);
+	}
+
+	private static String ardentArmour() {
+		return ChatUtils.translateToColor(
+				"&lArdent Aranarthium\n\n&r" +
+						"Provides the wearer with the status effects of &oStrength II, Resistance II,&r and an extra row of hearts.\n\nAdditionally yields increased damage using &lSwords."
+		);
+	}
+
+	private static String dwarvenArmour() {
+		return ChatUtils.translateToColor(
+				"&lDwarven Aranarthium\n\n&r" +
+						"Provides the wearer with the status effects of &oNight Vision&r, and improved ore and cluster drop rates.\n\nAdditionally yields increased damage using &lAxes &rand &lMaces."
+		);
+	}
+
+	private static String elvenArmour() {
+		return ChatUtils.translateToColor(
+				"&lElven Aranarthium\n\n&r" +
+						"Provides the wearer with the status effects of &oSpeed III&r and &oJump Boost II&r.\n\nAdditionally yields increased damage using &lBows."
+		);
+	}
+
+	private static String scorchedArmour() {
+		return ChatUtils.translateToColor(
+				"&lScorched Aranarthium\n\n&r" +
+						"Provides the wearer with the status effects of &oFire Resistance&r and Resistance I&r.\n\nAdditionally causes all melee damage to ignite targets, and the wearer is not targeted by Piglins."
+		);
+	}
+
+	private static String soulboundArmour() {
+		return ChatUtils.translateToColor(
+				"&lSoulbound Aranarthium\n\n&r" +
+						"Provides the wearer with no special status effects, but instead with the blessing of life-everlasting.\n\nThe wearer will have their full inventory persist across death."
+		);
+	}
 
 }
