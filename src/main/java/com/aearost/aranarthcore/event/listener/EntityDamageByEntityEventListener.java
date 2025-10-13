52861ca48d2de92a1ae611147ab8b0bbce44b9f1
aearost <liam.harbec13@gmail.com>
2025-10-12 20:57:31 -0400

    Added extra melee damage during the month of Faunivor

--- src/main/java/com/aearost/aranarthcore/event/listener/EntityDamageByEntityEventListener.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/EntityDamageByEntityEventListener.java
@@ -1,8 +1,11 @@
 package com.aearost.aranarthcore.event.listener;
 
 import com.aearost.aranarthcore.AranarthCore;
+import com.aearost.aranarthcore.enums.Month;
 import com.aearost.aranarthcore.event.mob.BabyZombiePreventVillagerDamage;
 import com.aearost.aranarthcore.event.player.ArenaPlayerKill;
+import com.aearost.aranarthcore.event.player.FaunivorExtraAttackDamage;
+import com.aearost.aranarthcore.utils.AranarthUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
@@ -22,8 +25,16 @@ public class EntityDamageByEntityEventListener implements Listener {
     public void onEntityDamageEntity(EntityDamageByEntityEvent e) {
         if (e.getDamager().getWorld().getName().equalsIgnoreCase("arena")) {
             new ArenaPlayerKill().execute(e);
-        } else if (e.getEntity() instanceof Villager villager) {
-            new BabyZombiePreventVillagerDamage().execute(e);
+        } else {
+            if (e.getEntity() instanceof Villager villager) {
+                new BabyZombiePreventVillagerDamage().execute(e);
+            }
+
+            if (AranarthUtils.getMonth() == Month.FAUNIVOR) {
+                new FaunivorExtraAttackDamage().execute(e);
+            }
         }
+
+
     }
 }
