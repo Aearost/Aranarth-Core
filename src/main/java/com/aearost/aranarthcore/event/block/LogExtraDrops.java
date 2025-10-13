0d96297ebc9c80d445a4fe89e418695dd7446491
aearost <liam.harbec13@gmail.com>
2025-10-12 20:07:29 -0400

    Added extra XP for logs during the month of Follivor Made it so extra wood drops during Follivor are now based on whether the actual log was placed or not

--- src/main/java/com/aearost/aranarthcore/event/block/LogExtraDrops.java
+++ src/main/java/com/aearost/aranarthcore/event/block/LogExtraDrops.java
@@ -1,8 +1,10 @@
 package com.aearost.aranarthcore.event.block;
 
+import com.gmail.nossr50.datatypes.player.McMMOPlayer;
+import com.gmail.nossr50.mcMMO;
+import com.gmail.nossr50.skills.woodcutting.WoodcuttingManager;
+import com.gmail.nossr50.util.EventUtils;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
-import org.bukkit.block.data.type.Leaves;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,18 +17,18 @@ public class LogExtraDrops {
 	public void execute(BlockBreakEvent e) {
 		if (e.getBlock().getType().name().endsWith("_LOG") && !e.getBlock().getType().name().endsWith("_STRIPPED_LOG")) {
 			Location loc = e.getBlock().getLocation();
-			// Circles through blocks until a leaf is reached
-			for (int y = loc.getBlockY() + 1; y < 255; y++) {
-				Block block = e.getBlock().getWorld().getBlockAt(loc.getBlockX(), y, loc.getBlockZ());
-				if (block.getBlockData() instanceof Leaves leaves) {
-					// Only has a chance to double if natural leaves
-					if (!leaves.isPersistent()) {
-						// 20% chance of logs doubling
-						if (new Random().nextInt(5) == 0) {
-							e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(e.getBlock().getType()));
-						}
-					}
-					return;
+			boolean isEligible = mcMMO.getChunkManager().isEligible(e.getBlock());
+			if (isEligible) {
+				// 33% chance of logs doubling
+				if (new Random().nextInt(3) == 0) {
+					e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(e.getBlock().getType()));
+				}
+
+				// mcMMO Herbalism XP gain is lost because of this
+				McMMOPlayer mcmmoPlayer = EventUtils.getMcMMOPlayer(e.getPlayer());
+				if (mcmmoPlayer != null) {
+					WoodcuttingManager woodcuttingManager = new WoodcuttingManager(mcmmoPlayer);
+					woodcuttingManager.processWoodcuttingBlockXP(e.getBlock());
 				}
 			}
 		}
