b8db6f3fd9df9e9af2d1c09c3bad15eb002d0ee9
aearost <liam.harbec13@gmail.com>
2025-10-12 23:06:25 -0400

    Removed prints from the ExpStore file Also added javadocs in its helper method

--- src/main/java/com/aearost/aranarthcore/event/player/ExpStore.java
+++ src/main/java/com/aearost/aranarthcore/event/player/ExpStore.java
@@ -1,6 +1,5 @@
 package com.aearost.aranarthcore.event.player;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -18,23 +17,13 @@ public class ExpStore {
 
     public void execute(PlayerInteractEvent e) {
         Player player = e.getPlayer();
-        Bukkit.getLogger().info("A");
         if (e.getHand() == EquipmentSlot.HAND) {
-            Bukkit.getLogger().info("B");
             if (player.getInventory().getItemInMainHand().getType() == Material.GLASS_BOTTLE) {
-                Bukkit.getLogger().info("C");
                 int amountToReduce = new Random().nextInt(10) + 10;
                 int totalExp = getTotalExp(player);
 
-                Bukkit.getLogger().info("Total: " + totalExp);
-                Bukkit.getLogger().info("EXP: " + player.getExp());
-                Bukkit.getLogger().info("EXP to Level: " + player.getExpToLevel());
-                Bukkit.getLogger().info("To reduce: " + amountToReduce);
-
                 if (totalExp >= amountToReduce) {
-                    Bukkit.getLogger().info("D");
                     if (player.isSneaking()) {
-                        Bukkit.getLogger().info("E");
                         // Reduces by a less than what is gained
                         amountToReduce = amountToReduce * -1;
                         player.giveExp(amountToReduce);
@@ -45,7 +34,6 @@ public class ExpStore {
                         HashMap<Integer, ItemStack> leftover = player.getInventory().addItem(new ItemStack(Material.EXPERIENCE_BOTTLE));
                         // If the player's inventory was full, drop it to the ground
                         if (!leftover.isEmpty()) {
-                            Bukkit.getLogger().info("F");
                             player.getLocation().getWorld().dropItemNaturally(player.getLocation(), leftover.get(0));
                         }
                         player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
@@ -55,6 +43,11 @@ public class ExpStore {
         }
     }
 
+    /**
+     * Helper method to provide the true total EXP a player has.
+     * @param player The player to be analyzed.
+     * @return The true total EXP.
+     */
     private int getTotalExp(Player player) {
         int level = player.getLevel();
         float progress = player.getExp(); // 0.0 - 1.0
