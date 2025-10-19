ccf33e300d6a5ec9643d372e0fc3fbc770eedc0c
aearost <liam.harbec13@gmail.com>
2025-10-19 00:54:35 -0400

    Updated chat format to support ranks and pronouns

--- src/main/java/com/aearost/aranarthcore/event/listener/misc/PlayerChatListener.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/misc/PlayerChatListener.java
@@ -1,6 +1,7 @@
 package com.aearost.aranarthcore.event.listener.misc;
 
 import com.aearost.aranarthcore.AranarthCore;
+import com.aearost.aranarthcore.enums.Pronouns;
 import com.aearost.aranarthcore.objects.AranarthPlayer;
 import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
@@ -16,7 +17,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
  * Based on <a href="https://www.spigotmc.org/threads/editing-message-to-player-from-asyncplayerchatevent.362198/">Spigot URL</a>
  */
 public class PlayerChatListener implements Listener {
-	
+
 	public PlayerChatListener(AranarthCore plugin) {
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
@@ -30,20 +31,96 @@ public class PlayerChatListener implements Listener {
                 p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 5f, 1f);
             }
         }
-        
+
         AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(event.getPlayer().getUniqueId());
         String nickname = aranarthPlayer.getNickname();
-        
+        String saintRank = getSaintRank(aranarthPlayer);
+        String councilRank = getCouncilRank(aranarthPlayer);
+        String rank = getRank(aranarthPlayer);
+
         String chatMessage = "⊰";
+
+        chatMessage += saintRank;
+        chatMessage += councilRank;
+        chatMessage += rank;
+
         if (!nickname.isEmpty()) {
         	chatMessage += ChatUtils.translateToColor(nickname + "&r");
         } else {
         	chatMessage += event.getPlayer().getName();
         }
-        chatMessage += "⊱ " + ChatUtils.translateToColor(event.getMessage() + "&r");
-        chatMessage = chatMessage.replaceAll("%", "%%");
-        
-        event.setFormat(chatMessage);
+
+        chatMessage += "⊱ &r";
+        chatMessage += event.getMessage();
+        event.setFormat(ChatUtils.translateToColor(chatMessage));
+
+//        chatMessage += "⊱ " + ChatUtils.translateToColor(event.getMessage() + "&r");
+//        chatMessage = chatMessage.replaceAll("%", "%%");
+
+//        event.setFormat(chatMessage);
     }
-	
+
+    /**
+     * Provides the String portion of the player's rank.
+     * @param aranarthPlayer The AranarthPlayer that is being analyzed.
+     * @return The String portion of the player's rank.
+     */
+    private String getRank(AranarthPlayer aranarthPlayer) {
+        int rank = aranarthPlayer.getRank();
+        if (aranarthPlayer.getPronouns() == Pronouns.MALE) {
+            switch (rank) {
+                case 1: return "&d[&aEsquire&d] &r";
+                case 2: return "&7[&fKnight&7] &r";
+                case 3: return "&5[&dBaron&5] &r";
+                case 4: return "&8[&7Count&8] &r";
+                case 5: return "&6[&eDuke&6] &r";
+                case 6: return "&6[&bPrince&6] &r";
+                case 7: return "&6[&9King&6] &r";
+                case 8: return "&6[&4Emperor&6] &r";
+            }
+        } else {
+            switch (rank) {
+                case 1: return "&d[&aEsquire&d] &r";
+                case 2: return "&7[&fKnight&7] &r";
+                case 3: return "&5[&dBaroness&5] &r";
+                case 4: return "&8[&7Countess&8] &r";
+                case 5: return "&6[&eDuchess&6] &r";
+                case 6: return "&6[&bPrincess&6] &r";
+                case 7: return "&6[&9Queen&6] &r";
+                case 8: return "&6[&4Empress&6] &r";
+            }
+        }
+        return "&8[&aPeasant&8] &r";
+    }
+
+    /**
+     * Provides the String portion of the player's Saint rank.
+     * @param aranarthPlayer The AranarthPlayer that is being analyzed.
+     * @return The String portion of the player's Saint rank.
+     */
+    private String getSaintRank(AranarthPlayer aranarthPlayer) {
+        int saintRank = aranarthPlayer.getSaintRank();
+        return switch (saintRank) {
+            case 1 -> "&b⚜&r";
+            case 2 -> "&e⚜&r";
+            case 3 -> "&c⚜&r";
+            default -> "";
+        };
+    }
+
+    /**
+     * Provides the String portion of the player's Council rank.
+     * @param aranarthPlayer The AranarthPlayer that is being analyzed.
+     * @return The String portion of the player's Council rank.
+     */
+    private String getCouncilRank(AranarthPlayer aranarthPlayer) {
+        int councilRank = aranarthPlayer.getCouncilRank();
+        return switch (councilRank) {
+            case 1 -> "&3۞ &r";
+            case 2 -> "&6۞ &r";
+            case 3 -> "&4۞ &r";
+            default -> "";
+        };
+    }
+
 }
