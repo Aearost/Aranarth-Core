63c36f81935a57050f60e22a77836a5f1d35cf69
aearost <liam.harbec13@gmail.com>
2025-10-30 23:15:26 -0400

    Centralized logic for getting solid block under player in one method in AranarthUtils

--- src/main/java/com/aearost/aranarthcore/commands/CommandSethome.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandSethome.java
@@ -6,8 +6,6 @@ import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -26,37 +24,7 @@ public class CommandSethome {
 			int playerMaxHomeCount = AranarthUtils.getMaxHomeNum(player);
 			if (aranarthPlayer.getHomes().size() < playerMaxHomeCount) {
 				if (args.length >= 2) {
-					Location loc = player.getLocation();
-					World world = loc.getWorld();
-
-					// Start from the player's current Y position and go downward
-					int x = loc.getBlockX();
-					int z = loc.getBlockZ();
-					int y = loc.getBlockY();
-
-					Block solidBlock = null;
-
-					for (int currentY = y; currentY >= world.getMinHeight(); currentY--) {
-						Block currentBlock = world.getBlockAt(x, currentY, z);
-						Material type = currentBlock.getType();
-
-						// Check if this block is solid and not water/lava
-						if (type.isSolid() && type != Material.WATER && type != Material.LAVA) {
-							solidBlock = currentBlock;
-							break;
-						}
-					}
-
-					// If a solid block was found, place the player just above it
-					Location surfaceLoc;
-					if (solidBlock != null) {
-						surfaceLoc = solidBlock.getLocation().add(0.0, 1, 0.0); // Center and place feet above
-						surfaceLoc.setYaw(loc.getYaw());
-						surfaceLoc.setPitch(loc.getPitch());
-					} else {
-						player.sendMessage(ChatUtils.chatMessage("&cYou cannot set a home here!"));
-						return true;
-					}
+					Location loc = AranarthUtils.getSolidBlockUnderneathPlayer(player);
 
 					// Construct the home name from args
 					StringBuilder homeNameBuilder = new StringBuilder();
@@ -92,7 +60,7 @@ public class CommandSethome {
 					}
 
 					// Create the home at the computed surface location
-					Home home = new Home(homeName, surfaceLoc, Material.BARRIER);
+					Home home = new Home(homeName, loc, Material.BARRIER);
 					AranarthUtils.addPlayerHome(player, home);
 					player.sendMessage(ChatUtils.chatMessage("&7You have added the home &e" + homeName));
 					return true;
