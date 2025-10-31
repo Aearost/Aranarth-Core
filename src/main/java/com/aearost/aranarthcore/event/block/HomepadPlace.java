94dc86631c5334d2a4f971dffd57010b37bfd8c3
aearost <liam.harbec13@gmail.com>
2025-10-30 21:23:41 -0400

    Restricted the crafting and placing of homepads to only SMP

--- src/main/java/com/aearost/aranarthcore/event/block/HomepadPlace.java
+++ src/main/java/com/aearost/aranarthcore/event/block/HomepadPlace.java
@@ -1,6 +1,7 @@
 package com.aearost.aranarthcore.event.block;
 
 import com.aearost.aranarthcore.utils.AranarthUtils;
+import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Location;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -17,6 +18,11 @@ public class HomepadPlace {
 			ItemMeta meta = e.getItemInHand().getItemMeta();
 			if (meta.getPersistentDataContainer().has(HOMEPAD)) {
 				Location location = e.getBlockPlaced().getLocation();
+				if (!location.getWorld().getName().startsWith("smp")) {
+					e.getPlayer().sendMessage(ChatUtils.chatMessage("&cYou can only place this in the SMP!"));
+					e.setCancelled(true);
+					return;
+				}
 				AranarthUtils.addNewHome(location);
 			}
 		}
