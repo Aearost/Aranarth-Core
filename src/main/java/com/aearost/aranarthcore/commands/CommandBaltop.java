c3542ada3e7efd861b23abf602102c63f87427f9
aearost <liam.harbec13@gmail.com>
2025-11-02 19:04:15 -0500

    Added in /ac baltop from previous AranarthCore code and made slight tweaks

--- src/main/java/com/aearost/aranarthcore/commands/CommandBaltop.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandBaltop.java
@@ -1,7 +1,13 @@
 package com.aearost.aranarthcore.commands;
 
+import com.aearost.aranarthcore.objects.AranarthPlayer;
+import com.aearost.aranarthcore.objects.BalanceComparator;
+import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.command.CommandSender;
+
+import java.text.NumberFormat;
+import java.util.*;
 
 /**
  * Lists the players with the highest balances.
@@ -13,8 +19,72 @@ public class CommandBaltop {
 	 * @param args The arguments of the command.
 	 */
 	public static boolean onCommand(CommandSender sender, String[] args) {
-		sender.sendMessage(ChatUtils.chatMessage("&4This feature is not currently implemented."));
+		List<String> lines = baltopSetup();
+
+		double generalPageAmount = lines.size() / 10.0;
+		int totalPageNumber = (int) generalPageAmount;
+
+		// Adds one if there isn't exactly 10 players on the last page
+		if (generalPageAmount % 1 != 0) {
+			totalPageNumber++;
+		}
+
+		sender.sendMessage(ChatUtils.translateToColor("&8      - - - &6&lAranarth Balances &8- - -"));
+		int page = 1;
+
+		if (args.length > 1) {
+			try {
+				page = Integer.parseInt(args[1]);
+
+				// If it's an invalid input
+				if (page <= 0) {
+					throw new NumberFormatException();
+				}
+			} catch (NumberFormatException e) {
+				sender.sendMessage(ChatUtils.translateToColor("&cPlease enter a valid page number!"));
+				return true;
+			}
+
+			if (page > totalPageNumber) {
+				page = totalPageNumber;
+			}
+		}
+
+		sender.sendMessage(ChatUtils.translateToColor("&7Showing page &6" + page + " &7of &6" + totalPageNumber));
+
+		int line = (page * 10) - 10;
+		page++;
+
+		for (int i = 0; i < 10; i++) {
+			sender.sendMessage(ChatUtils.translateToColor(lines.get(line)));
+			line++;
+			if (lines.size() == line) {
+				break;
+			}
+		}
+
 		return true;
+	}
+
+	/**
+	 * Provides the full list of all players' balances.
+	 * @return The full list of all player's balances.
+	 */
+	private static List<String> baltopSetup() {
+		HashMap<UUID, AranarthPlayer> players = AranarthUtils.getAranarthPlayers();
+		List<AranarthPlayer> playersAsList = new ArrayList<>(players.values());
+		List<String> lines = new ArrayList<>();
+
+		playersAsList.sort(new BalanceComparator());
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+		int counter = 1;
+		for (AranarthPlayer aranarthPlayer : playersAsList) {
+			lines.add("&7" + counter + ". &e" + aranarthPlayer.getNickname() + ", &6" + formatter.format(aranarthPlayer.getBalance()));
+			counter++;
+		}
+
+		return lines;
 	}
 
 }
