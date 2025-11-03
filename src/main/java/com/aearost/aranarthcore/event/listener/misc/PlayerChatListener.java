ed0cebfafe414199c853adf1090ff4625b087c21
aearost <liam.harbec13@gmail.com>
2025-11-02 19:17:56 -0500

    Added in rank symbols to /ac baltop and /ac dominion info

--- src/main/java/com/aearost/aranarthcore/event/listener/misc/PlayerChatListener.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/misc/PlayerChatListener.java
@@ -1,7 +1,6 @@
 package com.aearost.aranarthcore.event.listener.misc;
 
 import com.aearost.aranarthcore.AranarthCore;
-import com.aearost.aranarthcore.enums.Pronouns;
 import com.aearost.aranarthcore.objects.AranarthPlayer;
 import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
@@ -43,17 +42,12 @@ public class PlayerChatListener implements Listener {
         }
 
         String nickname = aranarthPlayer.getNickname();
-        String saintRank = getSaintRank(aranarthPlayer);
-        String councilRank = getCouncilRank(aranarthPlayer);
-        String architectRank = getArchitectRank(aranarthPlayer);
-        String rank = getRank(aranarthPlayer);
-
         String prefix = "⊰";
 
-        prefix += saintRank;
-        prefix += architectRank;
-        prefix += councilRank;
-        prefix += rank;
+        prefix += AranarthUtils.getSaintRank(aranarthPlayer);
+        prefix += AranarthUtils.getArchitectRank(aranarthPlayer);
+        prefix += AranarthUtils.getCouncilRank(aranarthPlayer);
+        prefix += AranarthUtils.getRank(aranarthPlayer);
 
         if (!nickname.isEmpty()) {
         	prefix += nickname + "&r";
@@ -73,81 +67,7 @@ public class PlayerChatListener implements Listener {
         e.setFormat(prefix + chatMessage);
     }
 
-    /**
-     * Provides the String portion of the player's rank.
-     * @param aranarthPlayer The AranarthPlayer that is being analyzed.
-     * @return The String portion of the player's rank.
-     */
-    private String getRank(AranarthPlayer aranarthPlayer) {
-        int rank = aranarthPlayer.getRank();
-        if (aranarthPlayer.getPronouns() == Pronouns.MALE) {
-            switch (rank) {
-                case 1: return "&d[&aEsquire&d] &r";
-                case 2: return "&7[&fKnight&7] &r";
-                case 3: return "&5[&dBaron&5] &r";
-                case 4: return "&8[&7Count&8] &r";
-                case 5: return "&6[&eDuke&6] &r";
-                case 6: return "&6[&bPrince&6] &r";
-                case 7: return "&6[&9King&6] &r";
-                case 8: return "&6[&4Emperor&6] &r";
-            }
-        } else {
-            switch (rank) {
-                case 1: return "&d[&aEsquire&d] &r";
-                case 2: return "&7[&fKnight&7] &r";
-                case 3: return "&5[&dBaroness&5] &r";
-                case 4: return "&8[&7Countess&8] &r";
-                case 5: return "&6[&eDuchess&6] &r";
-                case 6: return "&6[&bPrincess&6] &r";
-                case 7: return "&6[&9Queen&6] &r";
-                case 8: return "&6[&4Empress&6] &r";
-            }
-        }
-        return "&8[&aPeasant&8] &r";
-    }
 
-    /**
-     * Provides the String portion of the player's Saint rank.
-     * @param aranarthPlayer The AranarthPlayer that is being analyzed.
-     * @return The String portion of the player's Saint rank.
-     */
-    private String getSaintRank(AranarthPlayer aranarthPlayer) {
-        int saintRank = aranarthPlayer.getSaintRank();
-        return switch (saintRank) {
-            case 1 -> "&b⚜&r";
-            case 2 -> "&e⚜&r";
-            case 3 -> "&c⚜&r";
-            default -> "";
-        };
-    }
-
-    /**
-     * Provides the String portion of the player's Council rank.
-     * @param aranarthPlayer The AranarthPlayer that is being analyzed.
-     * @return The String portion of the player's Council rank.
-     */
-    private String getCouncilRank(AranarthPlayer aranarthPlayer) {
-        int councilRank = aranarthPlayer.getCouncilRank();
-        return switch (councilRank) {
-            case 1 -> "&3۞ &r";
-            case 2 -> "&6۞ &r";
-            case 3 -> "&4۞ &r";
-            default -> "";
-        };
-    }
-
-    /**
-     * Provides the String portion of the player's Architect rank.
-     * @param aranarthPlayer The AranarthPlayer that is being analyzed.
-     * @return The String portion of the player's Architect rank.
-     */
-    private String getArchitectRank(AranarthPlayer aranarthPlayer) {
-        int architectRank = aranarthPlayer.getArchitectRank();
-        return switch (architectRank) {
-            case 1 -> "&a&l\uD83D\uDD28 &r"; // Hammer emoji
-            default -> "";
-        };
-    }
 
     /**
      * Confirms if the player is currently muted.
