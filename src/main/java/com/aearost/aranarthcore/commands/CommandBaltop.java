7f9c83a0a68cf68c262c854aa7ab2cb9effac960
aearost <liam.harbec13@gmail.com>
2025-11-08 12:43:21 -0500

    Optimized import statements

--- src/main/java/com/aearost/aranarthcore/commands/CommandBaltop.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandBaltop.java
@@ -7,7 +7,10 @@ import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.command.CommandSender;
 
 import java.text.NumberFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Lists the players with the highest balances.
