b84325ad1c01052b4dd62bcfef8354ecd793540d
aearost <liam.harbec13@gmail.com>
2025-11-27 13:00:15 -0500

    Fixed issue where the current avatar was null and was causing errors when joining and leaving and saving

--- src/main/java/com/aearost/aranarthcore/event/player/QuiverClick.java
+++ src/main/java/com/aearost/aranarthcore/event/player/QuiverClick.java
@@ -18,7 +18,7 @@ public class QuiverClick {
 			ItemMeta meta = e.getItem().getItemMeta();
 			if (meta.getPersistentDataContainer().has(QUIVER)) {
 				if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-					if (e.getPlayer().getWorld().getName().startsWith("world") || e.getPlayer().getWorld().getName().startsWith("smp")) {
+					if (e.getPlayer().getWorld().getName().startsWith("world") || e.getPlayer().getWorld().getName().startsWith("smp") || e.getPlayer().getWorld().getName().startsWith("resource")) {
 						e.setCancelled(true);
 						e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ITEM_BUNDLE_REMOVE_ONE, 1F, 0.8F);
 						GuiQuiver gui = new GuiQuiver(e.getPlayer());
