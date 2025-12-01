2151b0144af3d8683115b7a347edb43f3561c350
aearost <liam.harbec13@gmail.com>
2025-12-01 17:32:11 -0500

    Prevented damage and bending at the arena spawn

--- src/main/java/com/aearost/aranarthcore/event/listener/grouped/ArenaProtection.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/grouped/ArenaProtection.java
@@ -4,16 +4,22 @@ import com.aearost.aranarthcore.AranarthCore;
 import com.aearost.aranarthcore.objects.AranarthPlayer;
 import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
+import com.projectkorra.projectkorra.BendingPlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 /**
  * Handles preventing behaviour in the arena world.
@@ -42,17 +48,11 @@ public class ArenaProtection implements Listener {
 	 */
 	@EventHandler
 	public void onBlockBreak(final BlockBreakEvent e) {
-		if (e.getBlock().getWorld().getName().equalsIgnoreCase("arena")) {
+		if (isArenaSpawn(e.getBlock().getLocation())) {
 			AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(e.getPlayer().getUniqueId());
 			if (aranarthPlayer.getCouncilRank() != 3) {
-				int x = e.getBlock().getX();
-				int y = e.getBlock().getY();
-				int z = e.getBlock().getZ();
-
-				if ((x >= -4 && x <= 4) && (y >= 100 && y <= 111) && (z >= -4 && z <=4)) {
-					e.setCancelled(true);
-					e.getPlayer().sendMessage(ChatUtils.chatMessage("&cYou cannot break this!"));
-				}
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatUtils.chatMessage("&cYou cannot break this!"));
 			}
 		}
 	}
@@ -106,4 +106,93 @@ public class ArenaProtection implements Listener {
 		}
 	}
 
+	/**
+	 * Determines if the location is within the Arena spawn area.
+	 * @param loc The location.
+	 * @return Confirmation if the location is within the Arena spawn area.
+	 */
+	private boolean isArenaSpawn(Location loc) {
+		if (!loc.getWorld().getName().equals("arena")) {
+			return false;
+		}
+
+		int x = loc.getBlockX();
+		int y = loc.getBlockY();
+		int z = loc.getBlockZ();
+
+		return (x >= -4 && x <= 4) && (y >= 100 && y <= 111) && (z >= -4 && z <=4);
+	}
+
+	/**
+	 * Prevents players from dealing and taking damage at the arena spawn.
+	 */
+	@EventHandler
+	private void onDamage(EntityDamageEvent e) {
+		if (isArenaSpawn(e.getEntity().getLocation())) {
+			if (e.getDamageSource().getCausingEntity() instanceof Player attacker) {
+				attacker.sendMessage(ChatUtils.chatMessage("&7You cannot harm players here!"));
+			}
+			// Additionally prevents the target from being damaged, regardless of the source of damage
+			e.setCancelled(true);
+		}
+	}
+
+	/**
+	 * Automatically toggles the player's bending based on their teleportation to and from spawn.
+	 */
+	@EventHandler
+	public void onTeleportFromSpawn(PlayerTeleportEvent e) {
+		toggleBendingForLocation(e.getPlayer(), e.getFrom(), e.getTo());
+	}
+
+	/**
+	 * Handles the actual toggling of the player's bending based on movement or teleportation to and from spawn.
+	 * @param player The player.
+	 * @param from The player's previous location before the movement or teleportation.
+	 * @param to The player's previous location after the movement or teleportation.
+	 */
+	private void toggleBendingForLocation(Player player, Location from, Location to) {
+		boolean isFromArenaSpawn = isArenaSpawn(from.getBlock().getLocation());
+		boolean isToArenaSpawn = isArenaSpawn(to.getBlock().getLocation());
+
+		// Consistently keep bending disabled at the arena spawn
+		if (isArenaSpawn(to)) {
+			BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+			if (bPlayer != null) {
+				if (bPlayer.isToggled()) {
+					bPlayer.toggleBending();
+				}
+			}
+		}
+
+		// Leaving the arena spawn
+		if (!isToArenaSpawn) {
+			BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+			if (bPlayer != null) {
+				if (!bPlayer.isToggled()) {
+					bPlayer.toggleBending();
+					return;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Prevents players from enabling their bending at spawn.
+	 */
+	@EventHandler
+	public void onToggleBending(PlayerCommandPreprocessEvent e) {
+		Player player = e.getPlayer();
+		if (isArenaSpawn(player.getLocation())) {
+			String[] parts = e.getMessage().split(" ");
+			if (parts.length > 1) {
+				if (parts[0].startsWith("/b")) {
+					if (parts[1].startsWith("t")) {
+						e.setCancelled(true);
+						player.sendMessage(ChatUtils.chatMessage("&cYou cannot toggle your bending here!"));
+					}
+				}
+			}
+		}
+	}
 }
