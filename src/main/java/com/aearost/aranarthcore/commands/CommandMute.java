e624fd38be33ff10d4eabefd174bdfbf0871de40
aearost <liam.harbec13@gmail.com>
2025-12-04 00:58:05 -0500

    Added sound effects for warnings and mutes

--- src/main/java/com/aearost/aranarthcore/commands/CommandMute.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandMute.java
@@ -6,6 +6,7 @@ import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -140,6 +141,7 @@ public class CommandMute {
 			for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
 				if (onlinePlayer.getUniqueId().equals(Bukkit.getOfflinePlayer(args[1]).getUniqueId())) {
 					onlinePlayer.sendMessage(ChatUtils.chatMessage("&cYou have been muted!"));
+					onlinePlayer.playSound(onlinePlayer.getLocation(), Sound.ENTITY_GHAST_HURT, 1F, 1.1F);
 				}
 			}
 		}
