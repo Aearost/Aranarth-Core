365933fe1c9256e5a832ea210379a5f11b350945
aearost <liam.harbec13@gmail.com>
2025-12-06 15:18:17 -0500

    Patched bug where double mob drops included

--- src/main/java/com/aearost/aranarthcore/event/listener/grouped/BoostEffectsListener.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/grouped/BoostEffectsListener.java
@@ -19,8 +19,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -129,7 +133,19 @@ public class BoostEffectsListener implements Listener {
 		if (e.getDamageSource().getCausingEntity() != null) {
 			if (e.getDamageSource().getCausingEntity() instanceof Player) {
 				if (!(e.getEntity() instanceof Player)) {
+					EntityEquipment equipment = e.getEntity().getEquipment();
+					List<ItemStack> equipmentList = new ArrayList<>();
+                    equipmentList.addAll(Arrays.asList(equipment.getArmorContents()));
+					equipmentList.add(equipment.getItemInMainHand());
+					equipmentList.add(equipment.getItemInOffHand());
+
 					for (ItemStack drop : e.getDrops()) {
+						// Avoid duplication of held items or worn armor
+						if (equipmentList.contains(drop)) {
+							continue;
+						}
+
+						// Avoids duplication of saddles and armor on mounts
 						if (drop.getType() == Material.SADDLE || drop.getType().name().contains("_ARMOR")) {
 							continue;
 						}
