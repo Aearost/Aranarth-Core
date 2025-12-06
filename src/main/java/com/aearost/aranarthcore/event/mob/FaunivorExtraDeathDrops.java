4bd3a235366e5bc1f9ab983bcfccbb68de9202b6
aearost <liam.harbec13@gmail.com>
2025-12-06 15:18:17 -0500

    Patched bug where double mob drops included

--- src/main/java/com/aearost/aranarthcore/event/mob/FaunivorExtraDeathDrops.java
+++ src/main/java/com/aearost/aranarthcore/event/mob/FaunivorExtraDeathDrops.java
@@ -2,8 +2,12 @@ package com.aearost.aranarthcore.event.mob;
 
 import org.bukkit.Material;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -11,7 +15,19 @@ import java.util.Random;
  */
 public class FaunivorExtraDeathDrops {
 	public void execute(EntityDeathEvent e) {
+		EntityEquipment equipment = e.getEntity().getEquipment();
+		List<ItemStack> equipmentList = new ArrayList<>();
+        equipmentList.addAll(Arrays.asList(equipment.getArmorContents()));
+		equipmentList.add(equipment.getItemInMainHand());
+		equipmentList.add(equipment.getItemInOffHand());
+
 		for (ItemStack drop : e.getDrops()) {
+			// Avoid duplication of held items or worn armor
+			if (equipmentList.contains(drop)) {
+				continue;
+			}
+
+			// Avoids duplication of saddles and armor on mounts
 			if (drop.getType() == Material.SADDLE || drop.getType().name().contains("_ARMOR")) {
 				continue;
 			}
