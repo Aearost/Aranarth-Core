6e1621b13fca9ba1f34a15125abc8627c3b9c0c5
aearost <liam.harbec13@gmail.com>
2025-12-08 21:16:32 -0500

    Added support to change the icon of warps and homes in-game

--- src/main/java/com/aearost/aranarthcore/event/player/GuiWarpClick.java
+++ src/main/java/com/aearost/aranarthcore/event/player/GuiWarpClick.java
@@ -4,6 +4,7 @@ import com.aearost.aranarthcore.objects.AranarthPlayer;
 import com.aearost.aranarthcore.objects.Home;
 import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
@@ -32,6 +33,23 @@ public class GuiWarpClick {
 				for (int i = 0; i < warps.size(); i++) {
 					if (e.getSlot() == i) {
 						Home warp = warps.get(i);
+
+						if (player.hasPermission("aranarth.warp.modify")) {
+							Material heldItem = e.getCursor().getType();
+							// If the user is trying to update the icon of a home
+							if (heldItem != Material.AIR) {
+								e.setCancelled(true);
+								if (heldItem == warp.getIcon()) {
+									player.sendMessage(ChatUtils.chatMessage("&cThis warp already uses that icon!"));
+								} else {
+									AranarthUtils.updateWarp(warp.getName(), warp.getLocation(), heldItem);
+									player.sendMessage(ChatUtils.chatMessage(warp.getName() + "&7's icon is now &e" + ChatUtils.getFormattedItemName(heldItem.name())));
+								}
+								player.closeInventory();
+								return;
+							}
+						}
+
 						AranarthUtils.teleportPlayer(player, player.getLocation(), warp.getLocation());
 						player.sendMessage(ChatUtils.chatMessage("&7You have warped to &e" + warp.getName()));
 						player.closeInventory();
