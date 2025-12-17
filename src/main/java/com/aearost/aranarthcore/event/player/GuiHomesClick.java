9ec0c646ba9a04c8d18db205675caf354b34ef56
aearost <liam.harbec13@gmail.com>
2025-12-16 21:03:09 -0500

    Fixed issue where home and warp icons couldn't be updated with certain slots due to the inventory types being seen as the same

--- src/main/java/com/aearost/aranarthcore/event/player/GuiHomesClick.java
+++ src/main/java/com/aearost/aranarthcore/event/player/GuiHomesClick.java
@@ -7,6 +7,7 @@ import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 
 /**
  * Handles the teleport logic for homes.
@@ -27,32 +28,34 @@ public class GuiHomesClick {
 					return;
 				}
 
-				for (int i = 0; i < aranarthPlayer.getHomes().size(); i++) {
-					if (e.getSlot() == i) {
-						Home home = aranarthPlayer.getHomes().get(i);
+				if (e.getClickedInventory().getType() == InventoryType.CHEST) {
+					for (int i = 0; i < aranarthPlayer.getHomes().size(); i++) {
+						if (e.getSlot() == i) {
+							Home home = aranarthPlayer.getHomes().get(i);
 
-						Material heldItem = e.getCursor().getType();
-						// If the user is trying to update the icon of a home
-						if (heldItem != Material.AIR) {
-							e.setCancelled(true);
-							if (heldItem == home.getIcon()) {
-								player.sendMessage(ChatUtils.chatMessage("&cThis home already uses that icon!"));
+							Material heldItem = e.getCursor().getType();
+							// If the user is trying to update the icon of a home
+							if (heldItem != Material.AIR) {
+								e.setCancelled(true);
+								if (heldItem == home.getIcon()) {
+									player.sendMessage(ChatUtils.chatMessage("&cThis home already uses that icon!"));
+								} else {
+									AranarthUtils.updateHome(player, home.getName(), home.getLocation(), heldItem);
+									player.sendMessage(ChatUtils.chatMessage("&e" + home.getName() + "&7's icon is now &e" + ChatUtils.getFormattedItemName(heldItem.name())));
+								}
+								player.closeInventory();
+								return;
 							} else {
-								AranarthUtils.updateHome(player, home.getName(), home.getLocation(), heldItem);
-								player.sendMessage(ChatUtils.chatMessage(home.getName() + "&7's icon is now &e" + ChatUtils.getFormattedItemName(heldItem.name())));
+								AranarthUtils.teleportPlayer(player, player.getLocation(), home.getLocation());
+								player.sendMessage(ChatUtils.chatMessage("&7You have teleported to &e" + home.getName()));
+								player.closeInventory();
+								return;
 							}
-							player.closeInventory();
-							return;
-						} else {
-							AranarthUtils.teleportPlayer(player, player.getLocation(), home.getLocation());
-							player.sendMessage(ChatUtils.chatMessage("&7You have teleported to &e" + home.getName()));
-							player.closeInventory();
-							return;
 						}
 					}
+					player.closeInventory();
+					player.sendMessage(ChatUtils.chatMessage("&cSomething with wrong with teleporting to that home..."));
 				}
-				player.closeInventory();
-				player.sendMessage(ChatUtils.chatMessage("&cSomething with wrong with teleporting to that home..."));
 			}
 		}
 	}
