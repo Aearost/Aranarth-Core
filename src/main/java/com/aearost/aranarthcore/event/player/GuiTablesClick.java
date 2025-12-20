c40fcfcc1a9c114f3668af2c04e0be1367f3ba10
aearost <liam.harbec13@gmail.com>
2025-12-20 14:01:22 -0500

    Updated plugin to be on the Paper API and not Spigot Fixed issues where /ac tables was not actually creating useable tables

--- src/main/java/com/aearost/aranarthcore/event/player/GuiTablesClick.java
+++ src/main/java/com/aearost/aranarthcore/event/player/GuiTablesClick.java
@@ -1,79 +1,93 @@
 package com.aearost.aranarthcore.event.player;
 
 import com.aearost.aranarthcore.gui.GuiFletchingTable;
-import com.aearost.aranarthcore.utils.ChatUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.MenuType;
+
 
 /**
  * Handles the teleport logic for the different kinds of tables.
  */
 public class GuiTablesClick {
 	public void execute(InventoryClickEvent e) {
-		if (ChatUtils.stripColorFormatting(e.getView().getTitle()).equals("Tables")) {
-			// If the user did not click a slot
-			if (e.getClickedInventory() == null) {
-				return;
-			}
+		// If the user did not click a slot
+		if (e.getClickedInventory() == null) {
+			return;
+		}
 
+		if (e.getClickedInventory().getType() != InventoryType.PLAYER) {
 			if (e.getWhoClicked() instanceof Player player) {
 				// Crafting Table
 				if (e.getSlot() == 0) {
 					e.setCancelled(true);
-					Inventory gui = Bukkit.getServer().createInventory(player, InventoryType.WORKBENCH);
 					player.closeInventory();
-					player.openInventory(gui);
+					MenuType.CRAFTING.builder()
+							.checkReachable(false)
+							.build(player)
+							.open();
 				}
 				// Fletching Table
 				else if (e.getSlot() == 1) {
 					e.setCancelled(true);
+					player.closeInventory();
 					GuiFletchingTable gui = new GuiFletchingTable(player);
 					gui.openGui();
 				}
 				// Smithing Table
 				else if (e.getSlot() == 2) {
 					e.setCancelled(true);
-					Inventory gui = Bukkit.getServer().createInventory(player, InventoryType.SMITHING);
 					player.closeInventory();
-					player.openInventory(gui);
+					MenuType.SMITHING.builder()
+							.checkReachable(false)
+							.build(player)
+							.open();
 				}
 				// Cartography Table
 				else if (e.getSlot() == 3) {
 					e.setCancelled(true);
-					Inventory gui = Bukkit.getServer().createInventory(player, InventoryType.CARTOGRAPHY);
 					player.closeInventory();
-					player.openInventory(gui);
+					MenuType.CARTOGRAPHY_TABLE.builder()
+							.checkReachable(false)
+							.build(player)
+							.open();
 				}
 				// Loom
 				else if (e.getSlot() == 4) {
 					e.setCancelled(true);
-					Inventory gui = Bukkit.getServer().createInventory(player, InventoryType.LOOM);
 					player.closeInventory();
-					player.openInventory(gui);
+					MenuType.LOOM.builder()
+							.checkReachable(false)
+							.build(player)
+							.open();
 				}
 				// Anvil
 				else if (e.getSlot() == 5) {
 					e.setCancelled(true);
-					Inventory gui = Bukkit.getServer().createInventory(player, InventoryType.ANVIL);
 					player.closeInventory();
-					player.openInventory(gui);
+					MenuType.ANVIL.builder()
+							.checkReachable(false)
+							.build(player)
+							.open();
 				}
 				// Grindstone
 				else if (e.getSlot() == 6) {
 					e.setCancelled(true);
-					Inventory gui = Bukkit.getServer().createInventory(player, InventoryType.GRINDSTONE);
 					player.closeInventory();
-					player.openInventory(gui);
+					MenuType.GRINDSTONE.builder()
+							.checkReachable(false)
+							.build(player)
+							.open();
 				}
 				// Stonecutter
 				else if (e.getSlot() == 7) {
 					e.setCancelled(true);
-					Inventory gui = Bukkit.getServer().createInventory(player, InventoryType.STONECUTTER);
 					player.closeInventory();
-					player.openInventory(gui);
+					MenuType.STONECUTTER.builder()
+							.checkReachable(false)
+							.build(player)
+							.open();
 				}
 			}
 		}
