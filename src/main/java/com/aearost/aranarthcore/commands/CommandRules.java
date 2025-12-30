3e0473b536ada38e0b2e8aacbbf46bb1e8c224d8
aearost <liam.harbec13@gmail.com>
2025-12-29 20:36:04 -0500

    Updated the server rules to clarify limitations on farms Restructured the order of rules Doubled the amount of blocks being melted at a time Added amount of bending presets that different ranks can create

--- src/main/java/com/aearost/aranarthcore/commands/CommandRules.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandRules.java
@@ -15,15 +15,15 @@ public class CommandRules {
 	public static boolean onCommand(CommandSender sender, String[] args) {
 		sender.sendMessage(ChatUtils.translateToColor("&8      - - - &6&lAranarth Rules &8- - -"));
 		sender.sendMessage(ChatUtils.translateToColor("&8[&61&8] &7Be kind and respectful to everyone, regardless of race, orientation, gender, religion, etc."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&62&8] &7No inappropriate conversation topics or excessive swearing"));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&63&8] &7No inappropriate skins, builds, item names or nicknames"));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&64&8] &7Do not impersonate or disrespect Staff (the Council)"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&62&8] &7Do not spam chat, use caps lock, etc."));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&63&8] &7No inappropriate conversation topics or excessive swearing"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&64&8] &7No inappropriate skins, builds, item names or nicknames"));
 		sender.sendMessage(ChatUtils.translateToColor("&8[&65&8] &7No advertising or mentioning other servers"));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&66&8] &7No glitch abuse, hacking, lag machines, or any sort of abuse of the game"));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&67&8] &7No automatic or AFK farms, auto-clickers, or other advantageous tools"));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&68&8] &7Do not spam chat, use caps lock, etc."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&69&8] &7Do not grief the world's terrain unnecessarily - use the resource world instead"));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&610&8] &7Ask Staff (the Council) before using mods - only client-side cosmetic mods are permitted (OptiFine, Shaders, MiniHUD, etc.)"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&66&8] &7No glitch abuse, hacking, auto-clickers, lag machines, or any sort of abuse of the game"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&67&8] &7Ask Staff (the Council) before using mods - only client-side cosmetic mods are permitted (OptiFine, Shaders, MiniHUD, etc.)"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&68&8] &7Do not impersonate or disrespect Staff (the Council)"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&69&8] &7Only the use of manually harvested crop farms or spawner farms are permitted"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&610&8] &7Do not grief the world's terrain unnecessarily - use the resource world instead"));
 		return true;
 	}
 
