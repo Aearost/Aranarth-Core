41ec3d7e6d521cb7043d3ef094509a1abde7acbf
aearost <liam.harbec13@gmail.com>
2026-01-07 12:07:07 -0500

    Fixed duplication bug involving shulker boxes in the off-hand

--- src/main/java/com/aearost/aranarthcore/event/player/ShulkerClick.java
+++ src/main/java/com/aearost/aranarthcore/event/player/ShulkerClick.java
@@ -1,16 +1,16 @@
 package com.aearost.aranarthcore.event.player;
 
 import com.aearost.aranarthcore.gui.GuiShulker;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
-
-import java.util.Objects;
 
 /**
  * Handles opening the shulker box when right-clicking while holding it.
@@ -20,19 +20,22 @@ public class ShulkerClick {
 		if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			Player player = e.getPlayer();
 			if (e.getPlayer().isSneaking()) {
-				if (Objects.nonNull(e.getItem())) {
-					ItemStack is = e.getItem();
-					if (is.getItemMeta() instanceof BlockStateMeta im) {
-						if (im.getBlockState() instanceof ShulkerBox shulker) {
-							if (!player.hasPermission("aranarth.shulker")) {
+				if (e.getHand() == EquipmentSlot.HAND) {
+					ItemStack heldItem = player.getInventory().getItemInMainHand();
+					if (heldItem != null && heldItem.getType() != Material.AIR) {
+						if (heldItem.getItemMeta() instanceof BlockStateMeta im) {
+							if (im.getBlockState() instanceof ShulkerBox shulker) {
+								if (!player.hasPermission("aranarth.shulker")) {
+									return;
+								}
+
+								e.setCancelled(true);
+								player.playSound(player, Sound.BLOCK_SHULKER_BOX_OPEN, 1F, 1F);
+								Inventory shulkerInventory = shulker.getInventory();
+								GuiShulker gui = new GuiShulker(player, shulkerInventory);
+								gui.openGui();
 								return;
 							}
-
-							e.setCancelled(true);
-							player.playSound(player, Sound.BLOCK_SHULKER_BOX_OPEN, 1F, 1F);
-							Inventory shulkerInventory = shulker.getInventory();
-							GuiShulker gui = new GuiShulker(player, shulkerInventory);
-							gui.openGui();
 						}
 					}
 				}
