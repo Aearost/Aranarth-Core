049982b1e6fcbd4f19d105d71df2fe959772cd44
aearost <liam.harbec13@gmail.com>
2026-02-09 18:37:02 -0500

    Implemented the Sing goat horn to drop apples/god apple fragments when used #50

--- src/main/java/com/aearost/aranarthcore/event/player/GoatHornUse.java
+++ src/main/java/com/aearost/aranarthcore/event/player/GoatHornUse.java
@@ -1,13 +1,20 @@
 package com.aearost.aranarthcore.event.player;
 
+import com.aearost.aranarthcore.enums.Month;
+import com.aearost.aranarthcore.items.GodAppleFragment;
 import com.aearost.aranarthcore.objects.AranarthPlayer;
 import com.aearost.aranarthcore.utils.AranarthUtils;
+import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.MusicInstrumentMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.Random;
 
 /**
  * Handles logic when a player blows a Goat Horn.
@@ -81,7 +88,74 @@ public class GoatHornUse {
         player.addPotionEffect(effect);
     }
 
+    /**
+     * Shakes nearby trees and drops apples and god apple fragments from them.
+     * @param player The player.
+     */
     private void dropNearbyApples(Player player) {
+        if (player.getWorld().getName().equals("world") || player.getWorld().getName().equals("smp")
+                || player.getWorld().getName().equals("resource")) {
+            Location loc = player.getLocation();
+            Block below = loc.subtract(0, 1, 0).getBlock();
+            if (below.getType() == Material.GRASS_BLOCK || below.getType() == Material.DIRT
+                    || below.getType() == Material.SAND || below.getType() == Material.GRAVEL
+                    || below.getType() == Material.OAK_LOG || below.getType() == Material.BIRCH_LOG) {
+                // Iterate 20 blocks around the player, and 5 blocks above/below for each leaf
+                for (int x = loc.getBlockX() - 10; x < loc.getBlockX() + 10; x++) {
+                    for (int z = loc.getBlockZ() - 10; z < loc.getBlockZ() + 10; z++) {
+                        for (int y = loc.getBlockY() + 5; y > loc.getBlockY() - 5; y--) {
+                            Block block = loc.getWorld().getBlockAt(x, y, z);
+                            if (block.getType() == Material.OAK_LEAVES || block.getType() == Material.DARK_OAK_LEAVES) {
+                                Block belowBlock = block.getWorld().getBlockAt(x, y - 1, z);
+                                if (belowBlock.getType() == Material.AIR || belowBlock.getType() == Material.LEAF_LITTER) {
+                                    if (AranarthUtils.getMonth() != Month.SOLARVOR) {
+                                        // 2.5% chance of dropping an apple
+                                        if (new Random().nextInt(40) == 0) {
+                                            belowBlock.getLocation().getWorld().dropItemNaturally(belowBlock.getLocation(), new ItemStack(Material.APPLE));
+                                        }
+                                        // 0.25% chance of dropping a god apple fragment during normal months
+                                        else if (new Random().nextInt(400) == 0) {
+                                            belowBlock.getLocation().getWorld().dropItemNaturally(belowBlock.getLocation(), new GodAppleFragment().getItem());
+                                            for (Player nearby : Bukkit.getOnlinePlayers()) {
+                                                if (!belowBlock.getWorld().getName().equals(nearby.getWorld().getName())) {
+                                                    continue;
+                                                }
 
+                                                // If the player is within 48 blocks of the spawn location
+                                                if (belowBlock.getLocation().distance(nearby.getLocation()) <= 48) {
+                                                    nearby.sendMessage(ChatUtils.chatMessage("&7A god apple fragment has dropped nearby"));
+                                                }
+                                            }
+                                        }
+                                    }
+                                    // Increased God Apple Fragment drop rates
+                                    else {
+                                        // 5% chance of dropping an apple
+                                        if (new Random().nextInt(20) == 0) {
+                                            belowBlock.getLocation().getWorld().dropItemNaturally(belowBlock.getLocation(), new ItemStack(Material.APPLE));
+                                        }
+                                        // 0.5% chance of dropping a god apple fragment during Solarvor
+                                        else if (new Random().nextInt(20) == 0) {
+                                            belowBlock.getLocation().getWorld().dropItemNaturally(belowBlock.getLocation(), new GodAppleFragment().getItem());
+                                            for (Player nearby : Bukkit.getOnlinePlayers()) {
+                                                if (!belowBlock.getWorld().getName().equals(nearby.getWorld().getName())) {
+                                                    continue;
+                                                }
+
+                                                // If the player is within 48 blocks of the spawn location
+                                                if (belowBlock.getLocation().distance(nearby.getLocation()) <= 48) {
+                                                    nearby.sendMessage(ChatUtils.chatMessage("&7A god apple fragment has dropped nearby"));
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+
 }
