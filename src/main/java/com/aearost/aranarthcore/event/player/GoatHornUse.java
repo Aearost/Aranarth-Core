f4cddc580991bab0826b41e6f6aff7ad5699510f
aearost <liam.harbec13@gmail.com>
2026-02-09 21:49:05 -0500

    Fixed goat horns attempted to be used regardless of the click type #50

--- src/main/java/com/aearost/aranarthcore/event/player/GoatHornUse.java
+++ src/main/java/com/aearost/aranarthcore/event/player/GoatHornUse.java
@@ -8,7 +8,9 @@ import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.MusicInstrumentMeta;
 import org.bukkit.potion.PotionEffect;
@@ -37,43 +39,45 @@ public class GoatHornUse {
     public void execute(PlayerInteractEvent e) {
         Player player = e.getPlayer();
         if (player.getWorld().getName().startsWith("world") || player.getWorld().getName().startsWith("smp")) {
-            MusicInstrumentMeta meta = (MusicInstrumentMeta) e.getItem().getItemMeta();
-            player.setCooldown(e.getItem(), 1); // Mimics no cooldown
-            if (meta.getInstrument() == MusicInstrument.PONDER_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.PONDER_GOAT_HORN)) {
-                    applyHornPotionEffect(player, new PotionEffect(PotionEffectType.SPEED, 600, 4));
-                    applyHornPotionEffect(player, new PotionEffect(PotionEffectType.JUMP_BOOST, 600, 4));
-                }
-            } else if (meta.getInstrument() == MusicInstrument.SING_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.SING_GOAT_HORN)) {
-                    dropNearbyApples(player);
-                }
-            } else if (meta.getInstrument() == MusicInstrument.SEEK_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.SEEK_GOAT_HORN)) {
-                    applyHornPotionEffect(player, new PotionEffect(PotionEffectType.STRENGTH, 600, 2));
-                    // Will additionally take more damage as per HornSeekExtraDamage
-                }
-            } else if (meta.getInstrument() == MusicInstrument.FEEL_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.FEEL_GOAT_HORN)) {
-                    applyHornPotionEffect(player, new PotionEffect(PotionEffectType.RESISTANCE, 200, 1));
-                    applyHornPotionEffect(player, new PotionEffect(PotionEffectType.REGENERATION, 200, 1));
-                    applyHornPotionEffect(player, new PotionEffect(PotionEffectType.SLOWNESS, 200, 2));
-                }
-            } else if (meta.getInstrument() == MusicInstrument.ADMIRE_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.ADMIRE_GOAT_HORN)) {
+            if (e.getHand() == EquipmentSlot.HAND && (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR)) {
+                MusicInstrumentMeta meta = (MusicInstrumentMeta) e.getItem().getItemMeta();
+                player.setCooldown(e.getItem(), 1); // Mimics no cooldown
+                if (meta.getInstrument() == MusicInstrument.PONDER_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.PONDER_GOAT_HORN)) {
+                        applyHornPotionEffect(player, new PotionEffect(PotionEffectType.SPEED, 600, 4));
+                        applyHornPotionEffect(player, new PotionEffect(PotionEffectType.JUMP_BOOST, 600, 4));
+                    }
+                } else if (meta.getInstrument() == MusicInstrument.SING_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.SING_GOAT_HORN)) {
+                        dropNearbyApples(player);
+                    }
+                } else if (meta.getInstrument() == MusicInstrument.SEEK_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.SEEK_GOAT_HORN)) {
+                        applyHornPotionEffect(player, new PotionEffect(PotionEffectType.STRENGTH, 600, 2));
+                        // Will additionally take more damage as per HornSeekExtraDamage
+                    }
+                } else if (meta.getInstrument() == MusicInstrument.FEEL_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.FEEL_GOAT_HORN)) {
+                        applyHornPotionEffect(player, new PotionEffect(PotionEffectType.RESISTANCE, 200, 1));
+                        applyHornPotionEffect(player, new PotionEffect(PotionEffectType.REGENERATION, 200, 1));
+                        applyHornPotionEffect(player, new PotionEffect(PotionEffectType.SLOWNESS, 200, 2));
+                    }
+                } else if (meta.getInstrument() == MusicInstrument.ADMIRE_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.ADMIRE_GOAT_HORN)) {
 
-                }
-            } else if (meta.getInstrument() == MusicInstrument.CALL_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.CALL_GOAT_HORN)) {
+                    }
+                } else if (meta.getInstrument() == MusicInstrument.CALL_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.CALL_GOAT_HORN)) {
 
-                }
-            } else if (meta.getInstrument() == MusicInstrument.YEARN_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.YEARN_GOAT_HORN)) {
+                    }
+                } else if (meta.getInstrument() == MusicInstrument.YEARN_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.YEARN_GOAT_HORN)) {
 
-                }
-            } else if (meta.getInstrument() == MusicInstrument.DREAM_GOAT_HORN) {
-                if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.DREAM_GOAT_HORN)) {
-                    cleanseNegativeEffects(player);
+                    }
+                } else if (meta.getInstrument() == MusicInstrument.DREAM_GOAT_HORN) {
+                    if (AranarthUtils.canUseHornSuccessfully(player, MusicInstrument.DREAM_GOAT_HORN)) {
+                        cleanseNegativeEffects(player);
+                    }
                 }
             }
         }
