2540b8aa8b2544358ab011f20793f5df641b7e42
aearost <liam.harbec13@gmail.com>
2026-02-09 22:10:38 -0500

    Added random teleportation logic in Survival and Resource worlds

--- src/main/java/com/aearost/aranarthcore/commands/CommandSurvival.java
+++ src/main/java/com/aearost/aranarthcore/commands/CommandSurvival.java
@@ -5,9 +5,13 @@ import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachment;
+
+import java.util.Random;
 
 /**
  * Teleports the player to the Survival world, sharing the Survival inventory.
@@ -22,8 +26,21 @@ public class CommandSurvival {
 	public static boolean onCommand(CommandSender sender, String[] args) {
 		if (args.length == 1) {
 			if (sender instanceof Player player) {
-				Location survivalSpawn = new Location(Bukkit.getWorld("world"), -29.5, 75, -73.5, 0, 0);
-				AranarthUtils.teleportPlayer(player, player.getLocation(), survivalSpawn);
+				World world = Bukkit.getWorld("world");
+				Random random = new Random();
+				Location selectedLocation = null;
+				boolean isLocationFound = false;
+				while (!isLocationFound) {
+					int x = random.nextInt(24501) - 12250;
+					int z = random.nextInt(24501) - 12250;
+					if (world.getHighestBlockAt(x, z).getType() != Material.WATER) {
+						isLocationFound = true;
+						selectedLocation = world.getHighestBlockAt(x, z).getLocation();
+						selectedLocation.add(0, 1, 0);
+					}
+				}
+
+				AranarthUtils.teleportPlayer(player, player.getLocation(), selectedLocation);
 				player.sendMessage(ChatUtils.chatMessage("&7You have been teleported to &eSurvival"));
 
 				PermissionAttachment perms = player.addAttachment(AranarthCore.getInstance());
