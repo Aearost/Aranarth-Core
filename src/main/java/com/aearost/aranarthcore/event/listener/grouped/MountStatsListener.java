69b69589587084ed157a4ffe3fbae48e06f4d189
aearost <liam.harbec13@gmail.com>
2026-02-22 19:36:53 -0500

    Implemented functionality for bred mounts to take stats from both parents #110

--- src/main/java/com/aearost/aranarthcore/event/listener/grouped/MountStatsListener.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/grouped/MountStatsListener.java
@@ -2,19 +2,37 @@ package com.aearost.aranarthcore.event.listener.grouped;
 
 import com.aearost.aranarthcore.AranarthCore;
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Random;
 
 import static com.aearost.aranarthcore.objects.CustomKeys.*;
+import static org.bukkit.attribute.Attribute.JUMP_STRENGTH;
+import static org.bukkit.attribute.Attribute.MOVEMENT_SPEED;
 
 public class MountStatsListener implements Listener {
+
+	private final double minHorseHealth = 16;
+	private final double maxHorseHealth = 60;
+	private final double minHorseJump = 0.57;
+	private final double maxHorseJump = 1.28;
+	private final double minHorseSpeed = 0.19;
+	private final double maxHorseSpeed = 0.592417062;
+
+	private final double minCamelHealth = 20;
+	private final double maxCamelHealth = 80;
+	private final double minCamelJump = 0.382;
+	private final double maxCamelJump = 0.909;
+	private final double minCamelSpeed = 0.19;
+	private final double maxCamelSpeed = 0.428;
 
 	public MountStatsListener(AranarthCore plugin) {
 		Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -31,7 +49,7 @@ public class MountStatsListener implements Listener {
 			return;
 		}
 
-		if (!(e.getEntity() instanceof Camel camel)) {
+		if (!(e.getEntity() instanceof Camel || e.getEntity() instanceof CamelHusk)) {
 			AbstractHorse horse;
 			if (e.getEntity() instanceof Horse) {
 				horse = (Horse) e.getEntity();
@@ -48,22 +66,22 @@ public class MountStatsListener implements Listener {
 
 			// A maximum limit of 30 hearts (60 half-hearts) --> 60
 			// Will need a minimum of 8 hearts (16 half-hearts)
-			final int healthBracket = r.nextInt(10) + 1;
-			final int healthMin;
-			final int healthMax;
+			final double healthBracket = r.nextInt(10) + 1;
+			final double healthMin;
+			final double healthMax;
 			if (healthBracket < 5) {
-				healthMin = 16;
+				healthMin = minHorseHealth;
 				healthMax = 34;
 			} else if (healthBracket < 9) {
 				healthMin = 35;
 				healthMax = 44;
 			} else {
 				healthMin = 45;
-				healthMax = 60;
+				healthMax = maxHorseHealth;
 			}
-			final int healthValue = r.nextInt((healthMax - healthMin) + 1) + healthMin;
+			final double healthValue = r.nextInt((int) ((healthMax - healthMin) + 1)) + healthMin;
 			horse.getAttribute(Attribute.MAX_HEALTH).setBaseValue(healthValue);
-			horse.getPersistentDataContainer().set(MOUNT_HEALTH, PersistentDataType.INTEGER, healthValue);
+			horse.getPersistentDataContainer().set(MOUNT_HEALTH, PersistentDataType.DOUBLE, healthValue);
 
 			// A maximum limit of 8 blocks of jump --> 1.28
 			// A minimum limit of 2 blocks of jump --> 0.57
@@ -71,17 +89,17 @@ public class MountStatsListener implements Listener {
 			final double jumpMin;
 			final double jumpMax;
 			if (jumpBracket < 5) {
-				jumpMin = 0.57;
+				jumpMin = minHorseJump;
 				jumpMax = 0.84;
 			} else if (jumpBracket < 9) {
 				jumpMin = 0.85;
 				jumpMax = 0.99;
 			} else {
 				jumpMin = 1.00;
-				jumpMax = 1.28;
+				jumpMax = maxHorseJump;
 			}
 			final double jumpValue = jumpMin + (jumpMax - jumpMin) * r.nextDouble();
-			horse.getAttribute(Attribute.JUMP_STRENGTH).setBaseValue(jumpValue);
+			horse.getAttribute(JUMP_STRENGTH).setBaseValue(jumpValue);
 			horse.getPersistentDataContainer().set(MOUNT_JUMP, PersistentDataType.DOUBLE, jumpValue);
 
 			// A maximum limit of 25 m/s --> 0.592417062
@@ -90,14 +108,14 @@ public class MountStatsListener implements Listener {
 			final double speedMin;
 			final double speedMax;
 			if (speedBracket < 5) {
-				speedMin = 0.19;
+				speedMin = minHorseSpeed;
 				speedMax = 0.24;
 			} else if (speedBracket < 9) {
 				speedMin = 0.25;
 				speedMax = 0.44;
 			} else {
 				speedMin = 0.45;
-				speedMax = 0.592417062;
+				speedMax = maxHorseSpeed;
 			}
 			final double speedValue = speedMin + (speedMax - speedMin) * r.nextDouble();
 			horse.getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(speedValue);
@@ -110,25 +128,27 @@ public class MountStatsListener implements Listener {
 				horse.setHealth(horse.getAttribute(Attribute.MAX_HEALTH).getValue());
 			}
 		} else {
+			LivingEntity camel = e.getEntity();
 			Random r = new Random();
 
 			// A maximum limit of 40 hearts (80 half-hearts) --> 80
 			// Will need a minimum of 8 hearts (16 half-hearts)
-			final int healthBracket = r.nextInt(10) + 1;
-			final int healthMin;
-			final int healthMax;
+			final double healthBracket = r.nextInt(10) + 1;
+			final double healthMin;
+			final double healthMax;
 			if (healthBracket < 5) {
-				healthMin = 20;
+				healthMin = minCamelHealth;
 				healthMax = 34;
 			} else if (healthBracket < 9) {
 				healthMin = 35;
 				healthMax = 54;
 			} else {
 				healthMin = 55;
-				healthMax = 80;
+				healthMax = maxCamelHealth;
 			}
-			final int healthValue = r.nextInt((healthMax - healthMin) + 1) + healthMin;
+			final double healthValue = r.nextInt((int) ((healthMax - healthMin) + 1)) + healthMin;
 			camel.getAttribute(Attribute.MAX_HEALTH).setBaseValue(healthValue);
+			camel.getPersistentDataContainer().set(MOUNT_HEALTH, PersistentDataType.DOUBLE, healthValue);
 
 			// A maximum limit of 4.5 blocks of jump --> 0.909
 			// A minimum limit of 1 blocks of jump --> 0.382
@@ -136,17 +156,18 @@ public class MountStatsListener implements Listener {
 			final double jumpMin;
 			final double jumpMax;
 			if (jumpBracket < 5) {
-				jumpMin = 0.382;
+				jumpMin = minCamelJump;
 				jumpMax = 0.44;
 			} else if (jumpBracket < 9) {
 				jumpMin = 0.45;
 				jumpMax = 0.84;
 			} else {
 				jumpMin = 0.85;
-				jumpMax = 0.909;
+				jumpMax = maxCamelJump;
 			}
 			final double jumpValue = jumpMin + (jumpMax - jumpMin) * r.nextDouble();
-			camel.getAttribute(Attribute.JUMP_STRENGTH).setBaseValue(jumpValue);
+			camel.getAttribute(JUMP_STRENGTH).setBaseValue(jumpValue);
+			camel.getPersistentDataContainer().set(MOUNT_JUMP, PersistentDataType.DOUBLE, jumpValue);
 
 			// A maximum limit of 18 m/s --> 0.428
 			// A minimum limit of 8 m/s --> 0.19
@@ -154,30 +175,168 @@ public class MountStatsListener implements Listener {
 			final double speedMin;
 			final double speedMax;
 			if (speedBracket < 5) {
-				speedMin = 0.19;
+				speedMin = minCamelSpeed;
 				speedMax = 0.24;
 			} else if (speedBracket < 9) {
 				speedMin = 0.25;
 				speedMax = 0.34;
 			} else {
 				speedMin = 0.35;
-				speedMax = 0.428;
+				speedMax = maxCamelSpeed;
 			}
 			final double speedValue = speedMin + (speedMax - speedMin) * r.nextDouble();
 			camel.getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(speedValue);
+			camel.getPersistentDataContainer().set(MOUNT_SPEED, PersistentDataType.DOUBLE, speedValue);
 		}
 	}
 
+	/**
+	 * Handles updating the mount stats when one is bred in.
+	 * Takes stats and traits from both parents, with a chance of increasing and of decreasing the stats.
+	 * @param e The event.
+	 */
+	@EventHandler
+	public void onMountBreed(final EntityBreedEvent e) {
+		boolean isMount = false;
+		LivingEntity child = e.getEntity();
+		if (child instanceof AbstractHorse || child instanceof Camel || child instanceof CamelHusk) {
+			isMount = true;
+		}
+
+		if (isMount) {
+			LivingEntity father = e.getFather();
+			double fatherHealth = father.getAttribute(Attribute.MAX_HEALTH).getBaseValue();
+			double fatherJump = father.getAttribute(JUMP_STRENGTH).getBaseValue();
+			double fatherSpeed = father.getAttribute(Attribute.MOVEMENT_SPEED).getBaseValue();
+
+			LivingEntity mother = e.getMother();
+			double motherHealth = mother.getAttribute(Attribute.MAX_HEALTH).getBaseValue();
+			double motherJump = mother.getAttribute(JUMP_STRENGTH).getBaseValue();
+			double motherSpeed = mother.getAttribute(Attribute.MOVEMENT_SPEED).getBaseValue();
+
+			double childHealth = calculateValue(MOUNT_HEALTH, child, fatherHealth, motherHealth);
+			double childJump = calculateValue(MOUNT_JUMP, child, fatherJump, motherJump);
+			double childSpeed = calculateValue(MOUNT_SPEED, child, fatherSpeed, motherSpeed);
+
+			Bukkit.getLogger().info("Calculated health: " + childHealth);
+			Bukkit.getLogger().info("Calculated jump: " + childJump);
+			Bukkit.getLogger().info("Calculated speed: " + childSpeed);
+
+			child.getAttribute(Attribute.MAX_HEALTH).setBaseValue(childHealth);
+			child.getPersistentDataContainer().set(MOUNT_HEALTH, PersistentDataType.DOUBLE, childHealth);
+			child.getAttribute(JUMP_STRENGTH).setBaseValue(childJump);
+			child.getPersistentDataContainer().set(MOUNT_JUMP, PersistentDataType.DOUBLE, childJump);
+			child.getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(childSpeed);
+			child.getPersistentDataContainer().set(MOUNT_SPEED, PersistentDataType.DOUBLE, childSpeed);
+		}
+	}
+
+	/**
+	 * Provides a value that is either the same, slightly lower, or slightly higher than the two input values.
+	 * @param key The type of parameter.
+	 * @param child The LivingEntity that is being bred.
+	 * @param fatherValue The value that the father has.
+	 * @param motherValue The value that the mother has.
+	 * @return The calculated value.
+	 */
+	private double calculateValue(NamespacedKey key, LivingEntity child, double fatherValue, double motherValue) {
+		Random random = new Random();
+		Bukkit.getLogger().info("------------------");
+		Bukkit.getLogger().info("Stat: " + key.getKey());
+		Bukkit.getLogger().info("Father stat: " + fatherValue);
+		Bukkit.getLogger().info("Mother stat: " + motherValue);
+
+		boolean isDecreasing = random.nextInt(5) == 0;
+		if (isDecreasing) {
+			Bukkit.getLogger().info("Decreasing the stat");
+			if (child instanceof AbstractHorse horse) {
+				if (key == MOUNT_HEALTH) {
+					double healthAmountToReduce = random.nextInt(4);
+					double calculated = child.getAttribute(Attribute.MAX_HEALTH).getBaseValue() - healthAmountToReduce;
+					return calculated < minHorseHealth ? minHorseHealth : calculated;
+				} else if (key == MOUNT_JUMP) {
+					double jumpAmountToReduce = random.nextDouble(0.25);
+					double calculated = child.getAttribute(JUMP_STRENGTH).getBaseValue() - jumpAmountToReduce;
+					return calculated < minHorseJump ? minHorseJump : calculated;
+				} else if (key == MOUNT_SPEED) {
+					double speedAmountToReduce = random.nextDouble(0.05);
+					double calculated = child.getAttribute(MOVEMENT_SPEED).getBaseValue() - speedAmountToReduce;
+					return calculated < minHorseSpeed ? minHorseSpeed : calculated;
+				}
+			} else if (child instanceof Camel camel || child instanceof CamelHusk) {
+				if (key == MOUNT_HEALTH) {
+					double healthAmountToReduce = random.nextInt(4);
+					double calculated = child.getAttribute(Attribute.MAX_HEALTH).getBaseValue() - healthAmountToReduce;
+					return calculated < minCamelHealth ? minCamelHealth : calculated;
+				} else if (key == MOUNT_JUMP) {
+					double jumpAmountToReduce = random.nextDouble(0.25);
+					double calculated = child.getAttribute(JUMP_STRENGTH).getBaseValue() - jumpAmountToReduce;
+					return calculated < minCamelJump ? minCamelJump : calculated;
+				} else if (key == MOUNT_SPEED) {
+					double speedAmountToReduce = random.nextDouble(0.05);
+					double calculated = child.getAttribute(MOVEMENT_SPEED).getBaseValue() - speedAmountToReduce;
+					return calculated < minCamelSpeed ? minCamelSpeed : calculated;
+				}
+			}
+		} else {
+			boolean isIncreasing = random.nextInt(5) == 0;
+			if (isIncreasing) {
+				Bukkit.getLogger().info("Increasing the stat");
+				if (child instanceof AbstractHorse horse) {
+					if (key == MOUNT_HEALTH) {
+						double healthAmountToIncrease = random.nextInt(4);
+						double calculated = child.getAttribute(Attribute.MAX_HEALTH).getBaseValue() + healthAmountToIncrease;
+						return calculated > maxHorseHealth ? maxHorseHealth : calculated;
+					} else if (key == MOUNT_JUMP) {
+						double jumpAmountToIncrease = random.nextDouble(0.25);
+						double calculated = child.getAttribute(JUMP_STRENGTH).getBaseValue() + jumpAmountToIncrease;
+						return calculated > maxHorseJump ? maxHorseJump : calculated;
+					} else if (key == MOUNT_SPEED) {
+						double speedAmountToIncrease = random.nextDouble(0.05);
+						double calculated = child.getAttribute(MOVEMENT_SPEED).getBaseValue() + speedAmountToIncrease;
+						return calculated > maxHorseSpeed ? maxHorseSpeed : calculated;
+					}
+				} else if (child instanceof Camel camel || child instanceof CamelHusk) {
+					if (key == MOUNT_HEALTH) {
+						double healthAmountToIncrease = random.nextInt(4);
+						double calculated = child.getAttribute(Attribute.MAX_HEALTH).getBaseValue() + healthAmountToIncrease;
+						return calculated > maxCamelHealth ? maxCamelHealth : calculated;
+					} else if (key == MOUNT_JUMP) {
+						double jumpAmountToIncrease = random.nextDouble(0.25);
+						double calculated = child.getAttribute(JUMP_STRENGTH).getBaseValue() + jumpAmountToIncrease;
+						return calculated > maxCamelJump ? maxCamelJump : calculated;
+					} else if (key == MOUNT_SPEED) {
+						double speedAmountToIncrease = random.nextDouble(0.05);
+						double calculated = child.getAttribute(MOVEMENT_SPEED).getBaseValue() + speedAmountToIncrease;
+						return calculated > maxCamelSpeed ? maxCamelSpeed : calculated;
+					}
+				}
+			}
+		}
+
+		Bukkit.getLogger().info("Taking average");
+		// Provides the average of both stats if not decreasing or increasing
+		if (key == MOUNT_HEALTH) {
+			return (int) (fatherValue + motherValue) / 2;
+		} else {
+			return (fatherValue + motherValue) / 2;
+		}
+	}
+
+	/**
+	 * Handles logic to refresh the mount's stats based on their PersistentDataContainer attributes.
+	 * @param e The event.
+	 */
 	@EventHandler
 	public void onMount(VehicleEnterEvent e) {
 		if (e.getVehicle() instanceof AbstractHorse || e.getVehicle() instanceof Camel) {
-			if (e.getVehicle().getPersistentDataContainer().get(MOUNT_HEALTH, PersistentDataType.INTEGER) == null
+			if (e.getVehicle().getPersistentDataContainer().get(MOUNT_HEALTH, PersistentDataType.DOUBLE) == null
 				|| e.getVehicle().getPersistentDataContainer().get(MOUNT_JUMP, PersistentDataType.DOUBLE) == null
 				|| e.getVehicle().getPersistentDataContainer().get(MOUNT_SPEED, PersistentDataType.DOUBLE) == null) {
 				return;
 			}
 
-			int health = e.getVehicle().getPersistentDataContainer().get(MOUNT_HEALTH, PersistentDataType.INTEGER);
+			double health = e.getVehicle().getPersistentDataContainer().get(MOUNT_HEALTH, PersistentDataType.DOUBLE);
 			double jump = e.getVehicle().getPersistentDataContainer().get(MOUNT_JUMP, PersistentDataType.DOUBLE);
 			double speed = e.getVehicle().getPersistentDataContainer().get(MOUNT_SPEED, PersistentDataType.DOUBLE);
 		}
