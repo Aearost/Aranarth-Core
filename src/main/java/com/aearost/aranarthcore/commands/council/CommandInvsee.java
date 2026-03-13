0ca7d1cb3d6c4a03787cc43e996b74d910fd3a55
aearost <liam.harbec13@gmail.com>
2026-03-13 16:16:24 -0400

    Implemented /ac invsee

--- src/main/java/com/aearost/aranarthcore/commands/council/CommandInvsee.java
+++ src/main/java/com/aearost/aranarthcore/commands/council/CommandInvsee.java
@@ -1,6 +1,7 @@
 package com.aearost.aranarthcore.commands.council;
 
 import com.aearost.aranarthcore.utils.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -15,12 +16,27 @@ public class CommandInvsee {
 	 */
 	public static boolean onCommand(CommandSender sender, String[] args) {
 		if (sender instanceof Player player) {
-			if (player.hasPermission("aranarth.invsee")) {
-				sender.sendMessage(ChatUtils.chatMessage("&4This feature is not currently implemented."));
+			if (!player.hasPermission("aranarth.invsee")) {
+				sender.sendMessage(ChatUtils.chatMessage("&cYou do not have permission to execute this command!"));
 				return true;
 			}
-		}
-		return false;
-	}
 
+			if (args.length == 1) {
+				player.sendMessage(ChatUtils.chatMessage("&cInvalid syntax: &e/ac invsee <player>"));
+				return true;
+			} else {
+				Player target = Bukkit.getPlayer(args[1]);
+				if (target != null) {
+					player.openInventory(target.getInventory());
+					return true;
+				} else {
+					player.sendMessage(ChatUtils.chatMessage("&cThis player could not be found"));
+					return true;
+				}
+			}
+		} else {
+			sender.sendMessage(ChatUtils.chatMessage("&cYou must be a player to execute this command!"));
+			return true;
+		}
+	}
 }
