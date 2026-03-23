7395b7a26ea525172243f7bfd57e07dea016864f
aearost <liam.harbec13@gmail.com>
2026-03-23 11:34:21 -0400

    Prevented EarthTunnel and Collapse from being used in other Dominions

--- src/main/java/com/aearost/aranarthcore/event/listener/grouped/AranarthCoreBendingListener.java
+++ src/main/java/com/aearost/aranarthcore/event/listener/grouped/AranarthCoreBendingListener.java
@@ -3,12 +3,15 @@ package com.aearost.aranarthcore.event.listener.grouped;
 import com.aearost.aranarthcore.AranarthCore;
 import com.aearost.aranarthcore.abilities.airbending.AstralProjection;
 import com.aearost.aranarthcore.abilities.waterbending.VineWhip;
+import com.aearost.aranarthcore.objects.Dominion;
 import com.aearost.aranarthcore.utils.ChatUtils;
+import com.aearost.aranarthcore.utils.DominionUtils;
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.Element;
 import com.projectkorra.projectkorra.ability.*;
 import com.projectkorra.projectkorra.event.BendingReloadEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.GameMode;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
@@ -19,7 +22,9 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -85,7 +90,63 @@ public class AranarthCoreBendingListener implements Listener {
 					}
 				}
 			}
+			// Earthbending
+			else if (ability instanceof EarthAbility && bendingPlayer.isElementToggled(Element.EARTH)) {
+				if (abilityName.equalsIgnoreCase("earthtunnel") || abilityName.equalsIgnoreCase("collapse")) {
+					e.setCancelled(preventEarthAbility(player));
+				}
+			}
 		}
+	}
+
+	/**
+	 * Prevents the Earth ability from being used if they do not have access to the Dominion.
+	 * @param player The player using the ability.
+	 * @return Whether the player can use the ability.
+	 */
+	private boolean preventEarthAbility(Player player) {
+		List<Chunk> chunks = new ArrayList<>();
+		int chunkX = player.getLocation().getChunk().getX();
+		int chunkZ = player.getLocation().getChunk().getZ();
+		for (int x = chunkX - 1; x <= chunkX + 1; x++) {
+			for (int z = chunkZ - 1; z <= chunkZ + 1; z++) {
+				Chunk chunk = player.getWorld().getChunkAt(x, z);
+				chunks.add(chunk);
+			}
+		}
+
+		for (Chunk chunk : chunks) {
+			if (shouldCancelEarthAbility(DominionUtils.getDominionOfChunk(chunk), player)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Determines whether the EarthTunnel should be canceled.
+	 * @param dominion The Dominion of the chunk.
+	 * @param player The Player using the ability.
+	 * @return Whether the EarthTunnel should be canceled.
+	 */
+	private boolean shouldCancelEarthAbility(Dominion dominion, Player player) {
+		if (dominion == null) {
+			return false;
+		}
+
+		Dominion playerDominion = DominionUtils.getPlayerDominion(player.getUniqueId());
+		boolean areAllied = false;
+		for (int i = 0; i < dominion.getAllied().size(); i++) {
+			UUID uuid = dominion.getAllied().get(i);
+			Dominion alliedDominion = DominionUtils.getPlayerDominion(uuid);
+			if (alliedDominion.getLeader().equals(playerDominion.getLeader())) {
+				areAllied = true;
+				break;
+			}
+		}
+
+		boolean containsMember = dominion.getMembers().contains(player.getUniqueId());
+		return !dominion.getMembers().contains(player.getUniqueId()) && !areAllied;
 	}
 
 	//  Below for AstralProjection overrides
