a7f430c9f5a4d1c2f07c688b3a2e624570107d24
aearost <liam.harbec13@gmail.com>
2026-03-26 19:57:22 -0400

    Added sound effect when blacklist items are trashed

--- src/main/java/com/aearost/aranarthcore/event/player/BlacklistItemPickupPrevent.java
+++ src/main/java/com/aearost/aranarthcore/event/player/BlacklistItemPickupPrevent.java
@@ -2,10 +2,12 @@ package com.aearost.aranarthcore.event.player;
 
 import com.aearost.aranarthcore.objects.AranarthPlayer;
 import com.aearost.aranarthcore.utils.AranarthUtils;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Prevents players from picking up blacklisted items.
@@ -24,6 +26,9 @@ public class BlacklistItemPickupPrevent {
 				if (result == 1) {
 					e.getItem().remove();
 					e.getItem().setItemStack(null);
+					// 0.5 to 0.75
+					float pitch = ThreadLocalRandom.current().nextFloat(0.5F, 0.75F);
+					player.playSound(player, Sound.ENTITY_CHICKEN_EGG, 0.2F, pitch);
 				} else if (result == 0) {
 					e.setCancelled(true);
 				}
