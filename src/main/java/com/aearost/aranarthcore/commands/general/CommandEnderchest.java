8673ed48acfbad4a3962cceb148e81353a2e9512
aearost <liam.harbec13@gmail.com>
2026-04-18 18:51:41 -0400

    Added /enderchest <username> for council members to view players' enderchests

--- src/main/java/com/aearost/aranarthcore/commands/general/CommandEnderchest.java
+++ src/main/java/com/aearost/aranarthcore/commands/general/CommandEnderchest.java
@@ -1,6 +1,9 @@
 package com.aearost.aranarthcore.commands.general;
 
+import com.aearost.aranarthcore.objects.AranarthPlayer;
+import com.aearost.aranarthcore.utils.AranarthUtils;
 import com.aearost.aranarthcore.utils.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +11,7 @@ import org.bukkit.entity.Player;
 
 /**
  * Opens the player's ender chest remotely.
+ * Admins can also view and modify any online player's ender chest by specifying a username.
  */
 public class CommandEnderchest implements CommandExecutor {
 
@@ -20,18 +24,39 @@ public class CommandEnderchest implements CommandExecutor {
 	 */
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
-		if (sender instanceof Player player) {
+		if (!(sender instanceof Player player)) {
+			sender.sendMessage(ChatUtils.chatMessage("&cThis can only be executed in-game!"));
+			return true;
+		}
+
+		if (args.length == 0) {
 			if (!player.hasPermission("aranarth.enderchest")) {
 				player.sendMessage(ChatUtils.chatMessage("&cYou do not have permission to use this command!"));
 				return true;
 			}
-
 			player.openInventory(player.getEnderChest());
 			return true;
-		} else {
-			sender.sendMessage(ChatUtils.chatMessage("&cThis can only be executed in-game!"));
+		}
+
+		if (args.length == 1) {
+			AranarthPlayer aranarthPlayer = AranarthUtils.getPlayer(player.getUniqueId());
+			if (!aranarthPlayer.isInAdminMode()) {
+				player.sendMessage(ChatUtils.chatMessage("&cYou must be in admin mode to view other players' ender chests!"));
+				return true;
+			}
+
+			Player target = Bukkit.getPlayer(args[0]);
+			if (target == null) {
+				player.sendMessage(ChatUtils.chatMessage("&cThis player could not be found!"));
+				return true;
+			}
+
+			player.openInventory(target.getEnderChest());
 			return true;
 		}
+
+		player.sendMessage(ChatUtils.chatMessage("&cIncorrect syntax! /enderchest [username]"));
+		return true;
 	}
 
 }
