037b090684ad4a8bdd1770b2187831fa55f59f5c
aearost <liam.harbec13@gmail.com>
2026-04-22 21:01:41 -0400

    Changed how /vote displays

--- src/main/java/com/aearost/aranarthcore/commands/general/CommandVote.java
+++ src/main/java/com/aearost/aranarthcore/commands/general/CommandVote.java
@@ -64,13 +64,13 @@ public class CommandVote implements CommandExecutor {
 	 */
 	private void displayVoteLinks(Player player) {
 		player.sendMessage(ChatUtils.translateToColor("&8      - - - &6&lVote Links &8- - -"));
-		player.sendMessage(buildVoteLink("&7&lPlanet Minecraft: ", "https://tinyurl.com/5n7wezr9"));
-		player.sendMessage(buildVoteLink("&7&lMC Server List: ", "https://tinyurl.com/ynsp6zm2"));
-		player.sendMessage(buildVoteLink("&7&lMC Servers: ", "https://tinyurl.com/4r97nrsa"));
-		player.sendMessage(buildVoteLink("&7&lMineList: ", "https://tinyurl.com/f8wfzbj6"));
-		player.sendMessage(buildVoteLink("&7&lMinecraft MP: ", "https://tinyurl.com/3c8hfadt"));
-		player.sendMessage(buildVoteLink("&7&lTopG: ", "https://tinyurl.com/5c2yxkzj"));
-		player.sendMessage(buildVoteLink("&7&lMinecraft Buzz: ", "https://tinyurl.com/4ve6h5zn"));
+		player.sendMessage(buildVoteLink("&8&l[&6&l1&8&l] &e&l&oPlanet Minecraft", "https://tinyurl.com/5n7wezr9"));
+		player.sendMessage(buildVoteLink("&8&l[&6&l2&8&l] &7&l&oMC Server List", "https://tinyurl.com/ynsp6zm2"));
+		player.sendMessage(buildVoteLink("&8&l[&6&l3&8&l] &e&l&oMC Servers", "https://tinyurl.com/4r97nrsa"));
+		player.sendMessage(buildVoteLink("&8&l[&6&l4&8&l] &7&l&oMineList", "https://tinyurl.com/f8wfzbj6"));
+		player.sendMessage(buildVoteLink("&8&l[&6&l5&8&l] &e&l&oMinecraft MP", "https://tinyurl.com/3c8hfadt"));
+		player.sendMessage(buildVoteLink("&8&l[&6&l6&8&l] &7&l&oTopG", "https://tinyurl.com/5c2yxkzj"));
+		player.sendMessage(buildVoteLink("&8&l[&6&l7&8&l] &e&l&oMinecraft Buzz", "https://tinyurl.com/4ve6h5zn"));
 	}
 
 	/**
@@ -79,13 +79,31 @@ public class CommandVote implements CommandExecutor {
 	 * @param url The URL to display and open on click.
 	 */
 	private Component buildVoteLink(String label, String url) {
-		Component labelComponent = LegacyComponentSerializer.legacySection().deserialize(
-				ChatUtils.translateToColor(label));
-		Component urlComponent = ChatUtils.clickableUrl(
-				LegacyComponentSerializer.legacySection().deserialize(ChatUtils.translateToColor("&e" + url)),
-				ChatUtils.translateToColor("&7Click to open in browser"),
+//		Component labelComponent = LegacyComponentSerializer.legacySection().deserialize(
+//				ChatUtils.translateToColor(label));
+//		Component urlComponent = ChatUtils.clickableUrl(
+//				LegacyComponentSerializer.legacySection().deserialize(ChatUtils.translateToColor("&e" + url)),
+//				ChatUtils.translateToColor("&7Click to open in browser"),
+//				url
+//		);
+
+		String[] parts = label.split(" ");
+		String siteName = ChatUtils.stripColorFormatting(parts[1]);
+		if (parts.length > 2) {
+			siteName += " " + ChatUtils.stripColorFormatting(parts[2]);
+		}
+		if (parts.length > 3) {
+			siteName += " " + ChatUtils.stripColorFormatting(parts[3]);
+		}
+
+		Component voteComponent = ChatUtils.clickableUrl(
+				LegacyComponentSerializer.legacySection().deserialize(ChatUtils.translateToColor("&e" + label)),
+				ChatUtils.translateToColor("&7Click to open &e" + siteName + " &7in browser"),
 				url
 		);
-		return Component.empty().append(labelComponent).append(urlComponent);
+
+		return Component.empty().append(voteComponent);
+
+//		return Component.empty().append(labelComponent).append(urlComponent);
 	}
 }
