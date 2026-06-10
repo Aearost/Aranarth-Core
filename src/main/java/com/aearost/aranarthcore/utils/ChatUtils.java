e7d58914ba695d9c01cdf3dddeab80d4c392fb9d
aearost <liam.harbec13@gmail.com>
2026-06-10 13:17:24 -0400

    Added ping sound effect when receiving a private message

--- src/main/java/com/aearost/aranarthcore/utils/ChatUtils.java
+++ src/main/java/com/aearost/aranarthcore/utils/ChatUtils.java
@@ -538,6 +538,7 @@ public class ChatUtils {
 			Component targetComponent = LegacyComponentSerializer.legacySection().deserialize(ChatUtils.translateToColor(targetPrefixRaw))
 					.append(LegacyComponentSerializer.legacySection().deserialize(coloredFormattedMsg));
 			target.sendMessage(ChatUtils.clickableCommand(targetComponent, ChatUtils.translateToColor("&7Reply to &e" + aranarthPlayer.getNickname()), "/msg " + player.getName() + " ", true));
+			target.playSound(target, org.bukkit.Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 5f, 1f);
 
 			targetAranarthPlayer.setLastReceivedMessage(player.getUniqueId());
 			AranarthUtils.setPlayer(target.getUniqueId(), targetAranarthPlayer);
