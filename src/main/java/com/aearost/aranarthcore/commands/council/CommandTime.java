b9be25e73be9b0f6f89bb37f59e4c4f15cfd408b
aearost <liam.harbec13@gmail.com>
2026-06-24 21:58:35 -0400

    Added base time for /ac time

--- src/main/java/com/aearost/aranarthcore/commands/council/CommandTime.java
+++ src/main/java/com/aearost/aranarthcore/commands/council/CommandTime.java
@@ -25,35 +25,58 @@ public class CommandTime {
 			}
 		}
 
-		if (args.length >= 2) {
-			try {
-				long time = Long.parseLong(args[1]);
-				updateTime(time);
-				sender.sendMessage(ChatUtils.chatMessage("&7The time has been updated"));
-			} catch (NumberFormatException e) {
-				long time = -1;
-				if (args[1].equalsIgnoreCase("day")) {
-					time = 0;
-				} else if (args[1].equalsIgnoreCase("noon")) {
-					time = 6000;
-				} else if (args[1].equalsIgnoreCase("night")) {
-					time = 13000;
-				} else if (args[1].equalsIgnoreCase("midnight")) {
-					time = 18000;
-				}
-
-				if (time == -1) {
-					sender.sendMessage(ChatUtils.chatMessage("&cInvalid syntax: &e/ac time <time>"));
-				} else {
-					updateTime(time);
-					sender.sendMessage(ChatUtils.chatMessage("&7The time has been updated"));
-				}
-			}
-			return true;
-		} else {
-			sender.sendMessage(ChatUtils.chatMessage("&cInvalid syntax: &e/ac time <time>"));
+		if (args.length == 1) {
+			World survival = Bukkit.getWorld("world");
+			long ticks = survival != null ? survival.getTime() : 0;
+			sender.sendMessage(ChatUtils.chatMessage("&7Current time: &e" + ticks + " ticks &7(" + getTimeOfDay(ticks) + ")"));
 			return true;
 		}
+
+		try {
+			long time = Long.parseLong(args[1]);
+			updateTime(time);
+			sender.sendMessage(ChatUtils.chatMessage("&7The time has been updated"));
+		} catch (NumberFormatException e) {
+			long time = -1;
+			if (args[1].equalsIgnoreCase("day")) {
+				time = 0;
+			} else if (args[1].equalsIgnoreCase("noon")) {
+				time = 6000;
+			} else if (args[1].equalsIgnoreCase("night")) {
+				time = 13000;
+			} else if (args[1].equalsIgnoreCase("midnight")) {
+				time = 18000;
+			}
+
+			if (time == -1) {
+				sender.sendMessage(ChatUtils.chatMessage("&cInvalid syntax: &e/ac time <time>"));
+			} else {
+				updateTime(time);
+				sender.sendMessage(ChatUtils.chatMessage("&7The time has been updated"));
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Returns an approximate time-of-day label for the given tick value.
+	 * Minecraft day starts at tick 0 (dawn). One full day = 24000 ticks.
+	 * @param ticks The world time in ticks.
+	 * @return A human-readable time-of-day label.
+	 */
+	private static String getTimeOfDay(long ticks) {
+		long t = ticks % 24000;
+		if (t < 1000) return "Dawn";
+		if (t < 3000) return "Morning";
+		if (t < 6000) return "Late Morning";
+		if (t < 7000) return "Noon";
+		if (t < 9000) return "Afternoon";
+		if (t < 11000) return "Late Afternoon";
+		if (t < 12000) return "Dusk";
+		if (t < 13000) return "Evening";
+		if (t < 18000) return "Night";
+		if (t < 22000) return "Midnight";
+		return "Late Night";
 	}
 
 	/**
