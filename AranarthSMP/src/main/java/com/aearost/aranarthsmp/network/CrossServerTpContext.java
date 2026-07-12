89b2d7c2c74123e5c594440d0617935cfbe14ac1
aearost <liam.harbec13@gmail.com>
2026-07-12 15:04:09 -0400

    Removed the now redundant AranarthSMP module as the jar is used in both servers

--- AranarthSMP/src/main/java/com/aearost/aranarthsmp/network/CrossServerTpContext.java
+++
@@ -1,9 +0,0 @@
-package com.aearost.aranarthsmp.network;
-
-import java.util.UUID;
-
-/**
- * Context for a cross-server teleport request received from the survival server.
- */
-public record CrossServerTpContext(UUID remotePlayerUuid, String remotePlayerNickname,
-                                   String remoteServer, boolean isTpHere) {}
