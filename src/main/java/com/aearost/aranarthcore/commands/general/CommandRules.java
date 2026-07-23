c0c3c7f770f63497ec0dbc1113ffc778158fd750
aearost <liam.harbec13@gmail.com>
2026-07-23 17:54:11 -0400

    Updates to the server rules, namely the automated farms rule

--- src/main/java/com/aearost/aranarthcore/commands/general/CommandRules.java
+++ src/main/java/com/aearost/aranarthcore/commands/general/CommandRules.java
@@ -20,16 +20,16 @@ public class CommandRules implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
 		sender.sendMessage(ChatUtils.translateToColor("&8      - - - &6&lAranarth Rules &8- - -"));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&61&8] &eTreat everyone with respect. Harassment, discrimination, bullying, or personal attacks are not tolerated."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&62&8] &7Do not spam chat or intentionally disrupt it with excessive caps, flooding, or similar behavior."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&63&8] &eKeep chat appropriate for a 13+ community. Excessive swearing, sexual discussions, graphic content, or other inappropriate conversations are not allowed."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&64&8] &7Keep nicknames, skins, builds, and item names appropriate. Offensive or explicit content is prohibited."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&65&8] &eKeep destruction during Dominion raids limited. The deliberate or excessive destruction of Dominion blocks is prohibited."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&66&8] &7Do not unnecessarily damage or litter the overworld. Use the Resource World for large-scale gathering."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&67&8] &eDo not use hacks, exploits, auto-clickers, lag machines, or anything that provides an unfair advantage."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&68&8] &7Only client-side mods approved by the Council (staff) are permitted (i.e OptiFine, Shaders, MiniHUD, etc)."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&69&8] &eRespect the Council and moderation decisions. Do not impersonate staff or intentionally interfere with moderation."));
-		sender.sendMessage(ChatUtils.translateToColor("&8[&610&8] &7Small-scaled automatic crop, mob, and spawner farms are permitted with prior Council approval. Large-scaled farms are not."));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&61&8] &eTreat everyone with respect. Harassment, discrimination, bullying, or personal attacks are not tolerated"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&62&8] &7Do not spam chat or intentionally disrupt it with excessive caps, chat flooding, or similar behavior"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&63&8] &eKeep chat appropriate for a 13+ community. Excessive swearing, sexual discussions, graphic content, or other inappropriate conversations are not allowed"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&64&8] &7Keep nicknames, skins, builds, and item names appropriate. Offensive or explicit content is prohibited"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&65&8] &eKeep destruction during Dominion raids limited. The deliberate or excessive destruction of Dominion blocks is prohibited"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&66&8] &7Do not unnecessarily damage or litter the overworld. Use the Resource World for large-scale gathering"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&67&8] &eDo not use hacks, exploits, auto-clickers, lag machines, or anything that provides an unfair advantage"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&68&8] &7Only client-side mods approved by the Council (staff) are permitted (i.e OptiFine, Shaders, MiniHUD, Litematica, etc)"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&69&8] &eRespect the Council and moderation decisions. Do not impersonate staff or intentionally interfere with moderation"));
+		sender.sendMessage(ChatUtils.translateToColor("&8[&610&8] &7Only small-scaled automatic crop, mob, and spawner farms are permitted to be built"));
 		return true;
 	}
 
