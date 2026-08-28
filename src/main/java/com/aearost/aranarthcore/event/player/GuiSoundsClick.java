aa1690425072f534906c11fbaea7b2d396f83a72
aearost <liam.harbec13@gmail.com>
2026-08-28 10:12:59 -0400

    Removed Sound keyword from the /sounds GUI click logic

--- src/main/java/com/aearost/aranarthcore/event/player/GuiSoundsClick.java
+++ src/main/java/com/aearost/aranarthcore/event/player/GuiSoundsClick.java
@@ -39,112 +39,112 @@ public class GuiSoundsClick {
                 aranarthPlayer.setJoinSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Join sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.BELL, "&f&lJoin Sound", vol);
+                refreshGui(player, slot, Material.BELL, "&f&lJoin", vol);
             }
             case 10 -> {
                 int vol = adjustVolume(aranarthPlayer.getLeaveSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setLeaveSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Leave sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.NOTE_BLOCK, "&f&lLeave Sound", vol);
+                refreshGui(player, slot, Material.NOTE_BLOCK, "&f&lLeave", vol);
             }
             case 11 -> {
                 int vol = adjustVolume(aranarthPlayer.getVoteSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setVoteSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Vote sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, new KeyVote().getItem(), "&f&lVote Sound", vol);
+                refreshGui(player, slot, new KeyVote().getItem(), "&f&lVote", vol);
             }
             case 12 -> {
                 int vol = adjustVolume(aranarthPlayer.getCrateSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setCrateSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Crate sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.CHEST, "&f&lCrate Sound", vol);
+                refreshGui(player, slot, Material.CHEST, "&f&lCrate", vol);
             }
             case 13 -> {
                 int vol = adjustVolume(aranarthPlayer.getWeatherSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setWeatherSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Weather sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.WATER_BUCKET, "&f&lWeather Sound", vol);
+                refreshGui(player, slot, Material.WATER_BUCKET, "&f&lWeather", vol);
             }
             case 14 -> {
                 int vol = adjustVolume(aranarthPlayer.getNewDaySoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setNewDaySoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7New day sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.CLOCK, "&f&lNew Day Sound", vol);
+                refreshGui(player, slot, Material.CLOCK, "&f&lNew Day", vol);
             }
             case 15 -> {
                 int vol = adjustVolume(aranarthPlayer.getNewMonthSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setNewMonthSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7New month sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.FIREWORK_ROCKET, "&f&lNew Month Sound", vol);
+                refreshGui(player, slot, Material.FIREWORK_ROCKET, "&f&lNew Month", vol);
             }
             case 16 -> {
                 int vol = adjustVolume(aranarthPlayer.getPrivateMsgSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setPrivateMsgSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Message sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.PAPER, "&f&lMessage Sound", vol);
+                refreshGui(player, slot, Material.PAPER, "&f&lMessage", vol);
             }
             case 17 -> {
                 int vol = adjustVolume(aranarthPlayer.getTeleportSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setTeleportSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Teleport sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.ENDER_PEARL, "&f&lTeleport Sound", vol);
+                refreshGui(player, slot, Material.ENDER_PEARL, "&f&lTeleport", vol);
             }
             case 18 -> {
                 int vol = adjustVolume(aranarthPlayer.getAvatarSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setAvatarSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Avatar sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.NETHER_STAR, "&f&lAvatar Sound", vol);
+                refreshGui(player, slot, Material.NETHER_STAR, "&f&lAvatar", vol);
             }
             case 19 -> {
                 int vol = adjustVolume(aranarthPlayer.getDominionSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setDominionSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Dominion sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.IRON_SWORD, "&f&lDominion Sound", vol);
+                refreshGui(player, slot, Material.IRON_SWORD, "&f&lDominion", vol);
             }
             case 20 -> {
                 int vol = adjustVolume(aranarthPlayer.getAranarthiumSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setAranarthiumSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Aranarthium sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.AMETHYST_SHARD, "&f&lAranarthium Sound", vol);
+                refreshGui(player, slot, Material.AMETHYST_SHARD, "&f&lAranarthium", vol);
             }
             case 21 -> {
                 int vol = adjustVolume(aranarthPlayer.getChatGameSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setChatGameSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Chat game sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.BOOK, "&f&lChat Game Sound", vol);
+                refreshGui(player, slot, Material.BOOK, "&f&lChat Game", vol);
             }
             case 22 -> {
                 int vol = adjustVolume(aranarthPlayer.getChestSortSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setChestSortSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Chest sort sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.CHEST, "&f&lChest Sort Sound", vol);
+                refreshGui(player, slot, Material.CHEST, "&f&lChest Sort", vol);
             }
             case 23 -> {
                 int vol = adjustVolume(aranarthPlayer.getJobsSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setJobsSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Jobs sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.IRON_PICKAXE, "&f&lJobs Sound", vol);
+                refreshGui(player, slot, Material.IRON_PICKAXE, "&f&lJobs", vol);
             }
             case 24 -> {
                 int vol = adjustVolume(aranarthPlayer.getExpStoreSoundVolume(), e.isLeftClick(), e.isShiftClick());
                 aranarthPlayer.setExpStoreSoundVolume(vol);
                 AranarthUtils.setPlayer(player.getUniqueId(), aranarthPlayer);
                 player.sendMessage(ChatUtils.chatMessage("&7Exp store sound volume set to &e" + vol + "%"));
-                refreshGui(player, slot, Material.EXPERIENCE_BOTTLE, "&f&lExp Store Sound", vol);
+                refreshGui(player, slot, Material.EXPERIENCE_BOTTLE, "&f&lExp Store", vol);
             }
         }
     }
